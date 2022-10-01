42d86a6f568e99fc4d2b60352dc1939e25ddcd56
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-10-02 00:16:20 +0200

    Time: 134 ms (46.62%), Space: 15.2 MB (50.91%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {} #val => index
+        for  i,n in enumerate(nums): #The count of the current iteration
+#The value of the item at the current iteration
+            diff =target -n
+            if diff in hashmap:
+                return [hashmap[diff],i]
+            hashmap[n]=i
+        return
+    #note that there is only one solution in the array
