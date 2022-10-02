c5806695bd2c3ae152faf7d22feee62b66181532
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-10-02 17:05:37 +0200

    Time: 3 ms (64.34%), Space: 5.8 MB (94.27%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -1,5 +1,24 @@
 class Solution {
 public:
+    // int sum_of_squares(int num){
+    //     int sum=0;
+    //     while(num){
+    //         int digit=num%10;
+    //         sum+=digit*digit;
+    //         num/=10;
+    //     }
+    //     return sum;
+    // }
+    // bool isHappy(int n) {
+    //     unordered_set<int> visited;
+    //     while(visited.find(n)==visited.end()){ // if it reaches the end . that means n isn't in the visited numbers .
+    //         visited.insert(n);
+    //         n=sum_of_squares(n);
+    //         if(n==1)
+    //             return true;
+    //     }
+    //     return false;
+    // }
     int sum_of_squares(int num){
         int sum=0;
         while(num){
@@ -10,13 +29,17 @@ public:
         return sum;
     }
     bool isHappy(int n) {
-        unordered_set<int> visited;
-        while(visited.find(n)==visited.end()){ // if it reaches the end . that means n isn't in the visited numbers .
-            visited.insert(n);
-            n=sum_of_squares(n);
-            if(n==1)
+        int slow_sum = n;
+        int fast_sum = sum_of_squares(n);
+        if(slow_sum==1 || fast_sum==1)
+            return true;
+        while(slow_sum!=fast_sum){
+            slow_sum=sum_of_squares(slow_sum);
+            fast_sum=sum_of_squares(sum_of_squares(fast_sum));
+            if(slow_sum==1 || fast_sum==1)
                 return true;
         }
         return false;
+
     }
 };
