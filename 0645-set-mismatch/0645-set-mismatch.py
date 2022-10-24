50cede2d22545762b0536c440e98bc0b494b2e67
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-10-25 01:43:56 +0200

    Time: 220 ms (87.12%), Space: 15.4 MB (85.55%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,16 +1,26 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        Sum = 0
-        dictionary = set()
+#         Sum = 0
+#         dictionary = set()
+#         repeated = 0
+#         for num in nums:
+#             if num in dictionary:
+#                 repeated = num
+#             else:
+#                 dictionary.add(num)
+#             Sum+=num
+#         Sum-=repeated
+#         n = len(nums)
+#         missing = (n*(n+1))//2 - Sum
+#         return [repeated,missing]
         repeated = 0
-        for num in nums:
-            if num in dictionary:
-                repeated = num
+        Sum = 0
+        for i in range(len(nums)):
+            if nums[abs(nums[i])-1]<0:
+                repeated = abs(nums[i])
             else:
-                dictionary.add(num)
-            Sum+=num
-        Sum-=repeated
+                Sum+=abs(nums[i])
+                nums[abs(nums[i])-1] *=-1
         n = len(nums)
-        missing = (n*(n+1))//2 - Sum
+        missing = n*(n+1)//2 - Sum
         return [repeated,missing]
-        
