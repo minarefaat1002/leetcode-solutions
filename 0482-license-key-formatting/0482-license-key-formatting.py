a198e49ff29ea5b319ed261ec1ea1d0b3a448ff7
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-10-25 17:28:40 +0200

    Time: 48 ms (94.04%), Space: 15.1 MB (25.51%) - LeetHub

--- 0482-license-key-formatting/0482-license-key-formatting.py
+++ 0482-license-key-formatting/0482-license-key-formatting.py
@@ -1,14 +1,26 @@
 class Solution:
     def licenseKeyFormatting(self, s: str, k: int) -> str:
-        newS = ""
-        count=0
+        # newS = ""
+        # count=0
+        # for i in range(len(s)-1,-1,-1):
+        #     if s[i]!="-":
+        #         newS = s[i].upper()+newS
+        #         count+=1
+        #     if count == k:
+        #         newS = "-" + newS
+        #         count = 0
+        # if newS and newS[0] =="-":
+        #     return newS[1:]
+        # return newS
+        stack = []
+        count = 0
         for i in range(len(s)-1,-1,-1):
             if s[i]!="-":
-                newS = s[i].upper()+newS
                 count+=1
+                stack.append(s[i])
             if count == k:
-                newS = "-" + newS
                 count = 0
-        if newS and newS[0] =="-":
-            return newS[1:]
-        return newS
+                stack.append("-")
+        if stack and stack[-1]=="-":
+            stack.pop()
+        return "".join(stack).upper()[::-1]
