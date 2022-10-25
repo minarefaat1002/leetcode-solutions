18718247a15a420c5ed8830301f0d9c972ac295a
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-10-25 15:36:51 +0200

    Time: 5184 ms (56.75%), Space: 14 MB (16.78%) - LeetHub

--- 2443-sum-of-number-and-its-reverse/2443-sum-of-number-and-its-reverse.py
+++ 2443-sum-of-number-and-its-reverse/2443-sum-of-number-and-its-reverse.py
@@ -1,13 +1,22 @@
 class Solution:
     def sumOfNumberAndReverse(self, num: int) -> bool:
+        # def reverse(i):
+        #     reverse = 0
+        #     while i:
+        #         reverse = reverse*10 + i%10
+        #         i = i//10
+        #     return reverse
+        # for i in range(num+1):
+        #     Sum=i+reverse(i)
+        #     if Sum==num:
+        #         return True
         def reverse(i):
-            reverse = 0
-            while i:
-                reverse = reverse*10 + i%10
-                i = i//10
-            return reverse
+            reverse = ""
+            for digit in i:
+                reverse = digit + reverse
+            return int(reverse)
         for i in range(num+1):
-            Sum=i+reverse(i)
+            Sum=i+reverse(str(i))
             if Sum==num:
                 return True
-        
+        return False
