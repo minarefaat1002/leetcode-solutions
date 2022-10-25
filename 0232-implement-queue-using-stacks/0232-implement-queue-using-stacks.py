e88eff100d8db0861af70908cc2bca4faf42aff7
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-10-25 02:47:39 +0200

    Time: 26 ms (98.62%), Space: 14 MB (75.32%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.py
@@ -1,14 +1,14 @@
 class MyQueue:
 
     def __init__(self):
-        self.stack1 = []
         self.stack2 = []
     def push(self, x: int) -> None:
+        stack1 = []
         while self.stack2:
-            self.stack1.append(self.stack2.pop())
+            stack1.append(self.stack2.pop())
         self.stack2.append(x)
-        while self.stack1:
-            self.stack2.append(self.stack1.pop())
+        while stack1:
+            self.stack2.append(stack1.pop())
 
     def pop(self) -> int:
         return self.stack2.pop()
