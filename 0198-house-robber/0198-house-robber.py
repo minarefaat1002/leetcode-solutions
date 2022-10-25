9c23f80af41b1fa5534d01cb73dc212b93214ebd
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-10-25 19:30:51 +0200

    Time: 52 ms (65.88%), Space: 13.8 MB (65.59%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,8 +1,17 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        rob1,rob2=0,0
-        for n in nums:
-            temp = max(n+rob1,rob2)
-            rob1=rob2
-            rob2=temp
-        return rob2
+        # def dfs(i):
+        #     if i == len(nums)-2:
+        #         return max(nums[len(nums)-1],nums[len(nums)-2])
+        #     elif i == len(nums)-1:
+        #         return nums[i]
+        #     return max(nums[i]+dfs(i+2),dfs(i+1))
+        # return dfs(0)
+        # the above solution has time complexity O(2^n) and it's so slow ===> leads to time limit exeeded 
+        hashmap = {len(nums)-1:nums[len(nums)-1] , len(nums)-2:max(nums[len(nums)-2],nums[len(nums)-1])}
+        def dfs(i):
+            if i in hashmap:
+                return hashmap[i]
+            hashmap[i] = max(nums[i]+dfs(i+2),dfs(i+1))
+            return hashmap[i]
+        return dfs(0)
