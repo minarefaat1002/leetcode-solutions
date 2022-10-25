4efdd826ec667be06d949ddc6ad2ec3ded7af880
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-10-25 18:58:07 +0200

    Time: 264 ms (57.16%), Space: 14.8 MB (99.19%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # for i in range(len(numbers)):
+        #     for j in range(i+1,len(numbers)):
+        #         if numbers[i]+numbers[j] == target:
+        #             return [i+1,j+1]
+        # return -1
+        # the above solution will lead to time limit exeeded cause the time complexity is : O(n^2)
+        l = 0
+        r = len(numbers)-1
+        Sum = numbers[l]+numbers[r]
+        while l<r:
+            Sum = numbers[l]+numbers[r]
+            if Sum ==target:
+                return [l+1,r+1]
+            elif Sum > target:
+                r-=1
+            else:
+                l+=1
