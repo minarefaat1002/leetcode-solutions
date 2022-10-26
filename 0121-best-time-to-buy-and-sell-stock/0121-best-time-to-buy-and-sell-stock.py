47f255cdc8465250ec12036ed30974afccfac736
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-10-26 02:25:29 +0200

    Time: 1698 ms (69.24%), Space: 24.9 MB (85.52%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # maxProfit = 0
+        # for i in range(len(prices)):
+        #     for j in range(i+1,len(prices)):
+        #         if prices[j] > prices[i]:
+        #             maxProfit = max(maxProfit,prices[j]-prices[i])
+        # return maxProfit
+        # the above solution has time complexity of O(n^2)
+        minPrice = float('inf')
+        maxProfit = 0 
+        for price in prices:
+            minPrice = min(minPrice,price)
+            maxProfit = max(maxProfit,price-minPrice)
+        return maxProfit
