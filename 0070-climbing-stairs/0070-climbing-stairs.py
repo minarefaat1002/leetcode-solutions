d78f0127ecd444ca4725548bd5fc66ff657b267a
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-10-27 01:56:54 +0200

    Time: 64 ms (15.15%), Space: 13.9 MB (57.32%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -8,12 +8,22 @@ class Solution:
     #         return dfs(i+1) + dfs(i+2)
     #     return dfs(0)
     # # tiem complexity of this solution is O(2^n) and it will lead to time limit exeeded
-        hashmap = {n:1}
-        def dfs(i):
-            if i>n:
-                return 0
-            if i in hashmap:
-                return hashmap[i]
-            hashmap[i] = dfs(i+1) + dfs(i+2)
-            return hashmap[i]
-        return dfs(0)
+    #     hashmap = {n:1}
+    #     def dfs(i):
+    #         if i>n:
+    #             return 0
+    #         if i in hashmap:
+    #             return hashmap[i]
+    #         hashmap[i] = dfs(i+1) + dfs(i+2)
+    #         return hashmap[i]
+    #     return dfs(0)
+    # # the above solution is an improvement of the first solution and it has time complexity of O(n) and space complexity of O(n)
+        first = 1
+        if n == 1:
+            return first
+        second = 2
+        for i in range(3,n+1):
+            temp = second 
+            second = first + second 
+            first = temp
+        return second
