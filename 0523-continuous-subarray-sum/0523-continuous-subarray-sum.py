790ad710ae5e090b06209f3fba9ff88b63860a25
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-10-27 01:14:49 +0200

    Time: 2762 ms (12.36%), Space: 33.1 MB (73.35%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -0,0 +1,22 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        # for i in range(len(nums)-1):
+        #     Sum = nums[i] + nums[i+1]
+        #     if Sum %k == 0:
+        #         return True
+        #     for j in range(i+2,len(nums)):
+        #         Sum+=nums[j]
+        #         if Sum%k == 0:
+        #             return True
+        # return False
+    # the above solution time complexity is O(n^2) and it leads to time limit exeeded
+        hashmap = {0:-1}
+        prefixSum = 0
+        for i in range(len(nums)):
+            prefixSum+=nums[i]
+            if (prefixSum%k in hashmap):
+                if (i - hashmap[prefixSum%k] >= 2) :
+                    return True
+            else:
+                hashmap[prefixSum%k] = i
+        return False
