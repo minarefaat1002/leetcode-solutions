3ffb8b502a8aca6f484cdeeaef807001c18f77eb
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-10-26 02:18:11 +0200

    Time: 53 ms (76.42%), Space: 13.8 MB (79.50%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,21 +5,37 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        head = ListNode(0,None)
-        temp = head
+        # head = ListNode(0,None)
+        # temp = head
+        # while l1 and l2:
+        #     newNode = ListNode(0,None)
+        #     if l1.val < l2.val:
+        #         newNode.val = l1.val
+        #         l1 = l1.next
+        #     else:
+        #         newNode.val = l2.val
+        #         l2 = l2.next
+        #     temp.next = newNode
+        #     temp = temp.next
+        # if l1:
+        #     temp.next = l1
+        # if l2:
+        #     temp.next = l2
+        # return head.next
+        # the above solution has time complexity of O(n) and space complexity of O(n)
+        dummy = ListNode(0,None)
+        temp = dummy
         while l1 and l2:
-            newNode = ListNode(0,None)
             if l1.val < l2.val:
-                newNode.val = l1.val
+                temp.next = l1
                 l1 = l1.next
             else:
-                newNode.val = l2.val
+                temp.next = l2
                 l2 = l2.next
-            temp.next = newNode
             temp = temp.next
         if l1:
             temp.next = l1
         if l2:
             temp.next = l2
-        return head.next
-        
+        return dummy.next
+        # the above solution has time complexity of O(n) and constant space complexity
