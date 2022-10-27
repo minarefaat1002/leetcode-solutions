8603817d253f0b74f58373331c533c500a539ec0
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-10-27 17:13:45 +0200

    Time: 1601 ms (56.03%), Space: 38.8 MB (89.74%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -0,0 +1,49 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        fast =head
+        slow = head 
+        #find middle (slow)
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            
+        #reverse second half 
+        prev = None
+        while slow:
+            temp = slow.next
+            slow.next=prev
+            prev = slow
+            slow = temp
+        #check palindrome 
+        left,right = head,prev
+        while right:
+            if left.val != right.val:
+                return False
+            left = left.next
+            right = right.next
+        return True
+            
+    
+    
+    '''
+    another solution 
+    
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        nums =[]
+        while head:
+            nums.append(head.val)
+            head = head.next
+        l,r=0,len(nums)-1
+        while l<=r:
+            if nums[l] != nums[r]:
+                return False
+            l+=1
+            r-=1
+        return True
+        time complexity is O(N) and the space complexity if also O(N)
+        '''
