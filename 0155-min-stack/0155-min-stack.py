e3338b52785cbe6a482bd397cc167d26b67284f9
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-10-28 00:45:42 +0200

    Time: 80 ms (80.55%), Space: 17.9 MB (66.09%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -2,18 +2,28 @@ class MinStack:
 
     def __init__(self):
         self.stack =[]
-        self.minStack =[]
+        self.Min = float('inf')
 
     def push(self, val: int) -> None:
-        self.stack.append(val)
-        self.minStack.append(min(val,self.minStack[-1] if self.minStack else val))# الاف كوندشن هنا عشان تتاكد ان الستاك مش فاضي 
-
+        if not self.stack:
+            self.stack.append(val)
+            self.Min = val
+        elif val >= self.Min:
+            self.stack.append(val)
+        else:
+            self.stack.append(2*val - self.Min)
+            self.Min = val
     def pop(self) -> None:
-        self.stack.pop()
-        self.minStack.pop()
-
+        if self.stack[-1] >= self.Min:
+            self.stack.pop()
+        else:
+            self.Min = 2*self.Min - self.stack[-1]
+            self.stack.pop()
     def top(self) -> int:
-        return self.stack[-1]
+        if self.stack[-1] >= self.Min:
+            return self.stack[-1]
+        else:
+            return self.Min
 
     def getMin(self) -> int:
-        return self.minStack[-1]
+        return self.Min
