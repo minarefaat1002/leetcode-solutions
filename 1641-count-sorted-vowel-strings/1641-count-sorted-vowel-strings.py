7b78d21a3c69876fe495e955b864bfdfe7608d04
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-10-27 15:45:58 +0200

    Time: 45 ms (78.29%), Space: 13.9 MB (65.87%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
@@ -1,13 +1,5 @@
 class Solution:
     def countVowelStrings(self, n: int) -> int:
-        # dp = [[],[1,1,1,1,1]]
-        # for i in range(2,n+1):
-        #     dp.append([0,0,0,0,0])
-        #     for j in range(0,5):
-        #         for z in range(0,j+1):
-        #             dp[i][j] += dp[i-1][z]
-        # return sum(dp[n])
-# time complexity if O(n) and space complexity is O(n)
         dp = [[],[1,1,1,1,1]]
         for i in range(2,n+1):
             dp.append([0,0,0,0,0])
@@ -15,3 +7,4 @@ class Solution:
                 for z in range(0,j+1):
                     dp[i][j] += dp[i-1][z]
         return sum(dp[n])
+# time complexity if O(n) and space complexity is O(n)
