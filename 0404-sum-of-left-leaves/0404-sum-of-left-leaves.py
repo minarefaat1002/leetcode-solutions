e1a409a007139884b02163e9f25c0ee64a45a486
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-10-27 17:52:28 +0200

    Time: 52 ms (69.74%), Space: 14.7 MB (71.84%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
@@ -8,9 +8,6 @@ class Solution:
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0 
-        if not root.left and not root.right:
-            return 0
-        Sum =[0]
         def dfs(root,isLeft):
             if not root:
                 return 0
@@ -21,3 +18,32 @@ class Solution:
                     return 0
             return dfs(root.left,True) + dfs(root.right,False)
         return dfs(root,False)
+"""
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        Sum = [0]
+        if not root:
+            return 0 
+        if not root.left and not root.right:
+            return 0
+        Sum =[0]
+        def dfs(root,isLeft):
+            if not root:
+                return
+            if not root.left and not root.right:
+                if isLeft:
+                    Sum[0]+=root.val
+                    return
+                else:
+                    return
+            dfs(root.left,True)
+            dfs(root.right,False)
+        dfs(root,False)
+        this is another solution
+"""
