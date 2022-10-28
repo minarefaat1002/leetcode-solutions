1d97571c71e80ea4be27f78ebdd1d8150bd1b2e2
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-10-28 20:43:10 +0200

    Time: 195 ms (63.88%), Space: 17.3 MB (18.71%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # [1,0]          1---->0 meaning that zero is a prereqisites
+        #map each course to prereq list
+        preMap = {i:[] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+        visited = [0]*numCourses
+        def dfs(crs):
+            if visited[crs]==1:
+                return True
+            elif visited[crs]==2:
+                return
+            visited[crs]=1
+            for adj in preMap[crs]:
+                if dfs(adj):
+                    return True
+            visited[crs]=2
+        for crs in range(numCourses):
+            if dfs(crs): return False
+        return True 
+    
