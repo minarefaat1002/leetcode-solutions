188d96ec6260f090e9493c2642a522b95f6e078c
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-10-28 14:56:31 +0200

    Time: 66 ms (29.25%), Space: 13.9 MB (59.29%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.py
+++ 0263-ugly-number/0263-ugly-number.py
@@ -3,11 +3,33 @@ class Solution:
         while n:
             if n==1:
                 return True
-            elif n%5 == 0:
-                n = n/5
-            elif n%3 == 0:
-                n = n/3
-            elif n%2 == 0:
-                n = n/2
+            elif n%5==0:
+                n=n/5
+            elif n%3==0:
+                n=n/3
+            elif n%2==0:
+                n=n/2
             else:
                 return False
+        # if n<=0:
+        #     return False
+        # while n!=1:
+        #     if fmod(n,2)==0:
+        #         n=n/2
+        #     elif fmod(n,3) ==0:
+        #         n=n/3
+        #     elif fmod(n,5) == 0:
+        #         n = n/5
+        #     else:
+        #         return False
+        # return True
+                
+        '''
+        another solution 
+        if n<=0
+            return False
+        for p in [2,3,5]:
+            while n%p ==0:
+                n = n//p        #// for integer division 
+        return n==1    #it means if n is equal to one then it will return true 
+        '''
