0d6712dab9f66315bb34765695bb97406e2d22c4
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-10-28 21:08:23 +0200

    Time: 598 ms (65.27%), Space: 20 MB (22.04%) - LeetHub

---
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numRollsToTarget(self, n: int, k: int, target: int) -> int:
+#         count=[0]
+#         def dp(target,n,k):
+#             if n==0 and target==0:
+#                 count[0]+=1
+#                 return
+#             elif n<0 or target<0:
+#                 return
+#             for i in range(1,k+1):
+#                 dp(target-i,n-1,k)
+#         dp(target,n,k)
+#         return count[0]
+# the above solution is so slow and has time limit exeeded .  to improve it we use caching 
+        hashMap = {(0,0):1}
+        count=[0]
+        def dp(target,n,k):
+            if (target,n) in hashMap:
+                return hashMap[(target,n)]
+            if n==0 and target==0:
+                return 1
+            elif n<0 or target<0:
+                return 0
+            Sum = 0
+            for i in range(1,k+1):
+                Sum +=dp(target-i,n-1,k)
+            hashMap[(target,n)] = Sum
+            return hashMap[(target,n)]
+        return dp(target,n,k) %(10**9+7)
