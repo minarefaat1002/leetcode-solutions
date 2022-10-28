8c7ed21deffd85c15748da89cb65b34dfe1de6fd
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-10-28 15:04:29 +0200

    Time: 66 ms (13.10%), Space: 13.9 MB (38.82%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -7,4 +7,14 @@ class Solution:
                     count+=1
                     i-=1
                 return count
-            
+    '''
+    another solution
+i,length = len(s)-1 , 0
+        while s[i] ==" ":
+            i-=1
+        while i>=0 and s[i] !=" ":
+            length +=1
+            i-=1
+        return length
+        
+    '''
