338c615e10194d30d5599d2c66c6f72ccbc48f6a
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-10-28 15:39:40 +0200

    Time: 67 ms (97.52%), Space: 13.9 MB (66.57%) - LeetHub

--- 1995-count-special-quadruplets/1995-count-special-quadruplets.py
+++ 1995-count-special-quadruplets/1995-count-special-quadruplets.py
@@ -1,16 +1,24 @@
 class Solution:
     def countQuadruplets(self, nums: List[int]) -> int:
+        # count = 0
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         for z in range(j+1,len(nums)):
+        #             for x in range(z+1,len(nums)):
+        #                 if nums[i]+nums[j]+nums[z]==nums[x]:
+        #                     count+=1
+        # return count 
+        # the above solution has time complexity of O(N^4) and it's fine for small inputs 
         res = 0
         l = len(nums)
+        count = {}
+        count[nums[len(nums)-1] - nums[len(nums)-2]] = 1
         
-        count = defaultdict(lambda: 0)
-        count[nums[l-1] - nums[l-2]] = 1
-        
-        for b in range(l - 3, 0, -1):
-            for a in range(b - 1, -1, -1):
-                res += count[nums[a] + nums[b]]
+        for i in range(len(nums) - 3, 0, -1):
+            for j in range(i - 1, -1, -1):
+                res = res + count.get(nums[j] + nums[i],0)
             
-            for x in range(l - 1, b, -1):
-                count[nums[x] - nums[b]] += 1
+            for x in range(len(nums) - 1, i, -1):
+                count[nums[x] - nums[i]] =count.get(nums[x] - nums[i],0) + 1
         
         return res
