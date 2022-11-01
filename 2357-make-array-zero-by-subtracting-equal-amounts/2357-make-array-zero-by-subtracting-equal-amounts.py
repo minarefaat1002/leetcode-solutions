4a294ed175b66ce29487fa129b0c808cc207ed89
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-11-01 18:14:07 +0200

    Time: 67 ms (44.02%), Space: 13.9 MB (12.22%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
@@ -1,10 +1,19 @@
+# class Solution:
+#     def minimumOperations(self, nums: List[int]) -> int:
+#         nums.sort()
+#         operations = 0
+#         for i in range(0,len(nums)):
+#             if nums[i] == 0:
+#                 continue
+#             if i==0 or nums[i] != nums[i-1]:
+#                 operations += 1
+#         return operations
+# this solution has time complexity of O(nlogn) and space complexity of O(N)
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        nums.sort()
-        operations = 0
-        for i in range(0,len(nums)):
-            if nums[i] == 0:
-                continue
-            if i==0 or nums[i] != nums[i-1]:
-                operations += 1
-        return operations
+        hashSet = set()
+        for num in nums:
+            if num != 0:
+                hashSet.add(num)
+        return len(hashSet)
+#this solution has time complexity of O(N) and space complexity of O(N)
