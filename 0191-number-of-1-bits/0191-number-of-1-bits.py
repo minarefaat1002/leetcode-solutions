428df18aad7a9f93edf020779692248596212c39
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-11-01 17:15:27 +0200

    Time: 62 ms (39.09%), Space: 13.8 MB (50.06%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -1,12 +1,20 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        # res = 0
-        # while n:
-        #     res += n%10
-        #     n = n//10
-        # return res
         res = 0
         while n:
             res += n%2
             n = n>>1
         return res
+    # it will take O(32)
+    # >>1  right shift by 1
+    '''
+    while n:
+        n = n&(n-1)
+        res+=1
+    return res
+    '''
+    '''
+    each time we and n with n-1 we get rid of the right most set bit 
+    ex : 1000001 ==> stubract 1 from it  ==> 1000000
+    ex: 1000000 ==> subtract 1 from it ===> 0111111 ==> note that we get rid of the right most set bit but there are many set bits appears at the right of our bit . but no problem because these bits will be removed when we and them in the next time 
+    '''
