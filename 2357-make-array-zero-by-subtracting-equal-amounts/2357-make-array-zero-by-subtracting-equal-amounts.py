4f3bb998b9be64a000e1607a7d0edd0469024b48
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-11-01 18:11:50 +0200

    Time: 62 ms (58.12%), Space: 13.9 MB (12.22%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
@@ -1,7 +1,10 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        hashSet = set()
-        for num in nums:
-            if num != 0:
-                hashSet.add(num)
-        return len(hashSet)
+        nums.sort()
+        operations = 0
+        for i in range(0,len(nums)):
+            if nums[i] == 0:
+                continue
+            if i==0 or nums[i] != nums[i-1]:
+                operations += 1
+        return operations
