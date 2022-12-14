5e49fdc35accd8a6c733008469a5455b17092859
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-12-15 01:26:33 +0200

    Time: 576 ms (95.82%), Space: 33 MB (7.07%) - LeetHub

---
+++ 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.py
@@ -0,0 +1,15 @@
+class Solution:
+    def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
+        # if even then 0
+        # if odd then num
+        len1 = len(nums1)
+        len2 = len(nums2)
+        Sum = 0
+        xor = 0
+        if len2%2 != 0:
+            for num in nums1:
+                xor = xor^num
+        if len1%2 !=0:
+            for num in nums2:
+                xor = xor^num
+        return xor
