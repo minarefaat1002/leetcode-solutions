a09a65efdb3b8b36e3909b589c5dc43a90b29f34
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-12-18 23:42:28 +0200

    Time: 139 ms (76.07%), Space: 14.3 MB (8.71%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.py
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.py
@@ -1,5 +1,9 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
+        # ans = [0]*len(nums)
+        # for i in range(len(nums)):
+        #     ans[i] = nums[nums[i]]
+        # return ans
         Max = max(nums)+1
         for i in range(len(nums)):
             nums[i] = ((nums[nums[i]%Max])%Max)*Max + nums[i]
