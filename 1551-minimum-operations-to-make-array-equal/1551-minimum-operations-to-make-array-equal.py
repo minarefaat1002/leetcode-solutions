30ec100ba5bb8701e255a0065cd41bd4356fd478
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-12-20 22:58:12 +0200

    Time: 180 ms (31.05%), Space: 13.9 MB (59.13%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.py
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.py
@@ -1,10 +1,7 @@
 class Solution:
     def minOperations(self, n: int) -> int:
-        Sum = 0
-        for i in range(n):
-            Sum += 2*i + 1
+        avg = int((n*(1 + 2*(n-1) + 1)/2)/n)
         operations = 0
-        avg = int(Sum/n)
         for i in range(n):
             if 2*i + 1 < avg:
                 operations+= avg - (2*i + 1)
