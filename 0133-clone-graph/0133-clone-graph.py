523b0135b3595a06016a9e69c8009092b522e72a
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-12-23 20:36:27 +0200

    Time: 37 ms (95.36%), Space: 14.3 MB (98.47%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -8,21 +8,33 @@ class Node:
 
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
+        # if not node:
+        #     return None
+        # newNode = Node(node.val,None)
+        # hashMap = {}
+        # visited = [False]*101
+        # hashMap[newNode.val] = newNode
+        # def dfs(newNode,node):
+        #     if visited[node.val]:
+        #         return
+        #     visited[node.val] = True
+        #     for neighbor in node.neighbors:
+        #         ne = Node(neighbor.val,None) if neighbor.val not in hashMap else hashMap[neighbor.val]
+        #         hashMap[ne.val] = ne
+        #         newNode.neighbors.append(ne)
+        #         dfs(ne,neighbor)
+        #     return newNode
+        # dfs(newNode,node)
+        # return newNode  
+        def dfs(node):
+            clone = Node(node.val)
+            hashMap[node.val] = clone
+            for neighbor in node.neighbors:
+                if neighbor.val not in hashMap:
+                    dfs(neighbor)
+                clone.neighbors.append(hashMap[neighbor.val])
+            return clone
         if not node:
             return None
-        newNode = Node(node.val,None)
         hashMap = {}
-        visited = [False]*101
-        hashMap[newNode.val] = newNode
-        def dfs(newNode,node):
-            if visited[node.val]:
-                return
-            visited[node.val] = True
-            for neighbor in node.neighbors:
-                ne = Node(neighbor.val,None) if neighbor.val not in hashMap else hashMap[neighbor.val]
-                hashMap[ne.val] = ne
-                newNode.neighbors.append(ne)
-                dfs(ne,neighbor)
-            return newNode
-        dfs(newNode,node)
-        return newNode    
+        return dfs(node)
