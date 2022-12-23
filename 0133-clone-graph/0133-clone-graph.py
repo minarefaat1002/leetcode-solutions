7a0b438e2ceb1b4c3ecc16e5d9f3c811ba86df94
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-12-23 20:29:47 +0200

    Time: 39 ms (92.19%), Space: 14.5 MB (27.10%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -12,17 +12,17 @@ class Solution:
             return None
         newNode = Node(node.val,None)
         hashMap = {}
+        visited = [False]*101
         hashMap[newNode.val] = newNode
         def dfs(newNode,node):
+            if visited[node.val]:
+                return
+            visited[node.val] = True
             for neighbor in node.neighbors:
-                if neighbor.val in hashMap:
-                    ne = hashMap[neighbor.val]
-                    newNode.neighbors.append(ne)
-                else:
-                    ne = Node(neighbor.val,None)
-                    hashMap[ne.val] = ne
-                    newNode.neighbors.append(ne)
-                    dfs(ne,neighbor)
+                ne = Node(neighbor.val,None) if neighbor.val not in hashMap else hashMap[neighbor.val]
+                hashMap[ne.val] = ne
+                newNode.neighbors.append(ne)
+                dfs(ne,neighbor)
             return newNode
         dfs(newNode,node)
         return newNode    
