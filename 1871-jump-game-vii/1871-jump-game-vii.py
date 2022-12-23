624f28c8a484aa7c61ca67035efc5e1d4a75168d
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-12-24 00:37:27 +0200

    Time: 415 ms (90.24%), Space: 18 MB (41.65%) - LeetHub

---
+++ 1871-jump-game-vii/1871-jump-game-vii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
+        # we solve it using bfs
+        q, farthest = deque([0]),0
+        while q:
+            i = q.popleft()
+            start = max(i+minJump,farthest+1)
+            for j in range(start,min(i+maxJump+1,len(s))):
+                if s[j] =="0":
+                    q.append(j)
+                    if j == len(s)-1:
+                        return True
+            farthest = i+maxJump
+        return False
+            
