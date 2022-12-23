69a7198516c42bbf7751a9aa81635e0ca824a920
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-12-24 00:38:26 +0200

    Time: 1327 ms (87.19%), Space: 28.5 MB (83.50%) - LeetHub

---
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxAlternatingSum(self, nums: List[int]) -> int:
+        sumeven, sumodd = 0 ,0
+        for i in range(len(nums)-1,-1,-1):
+            tmpeven = max(sumodd+nums[i],sumeven)
+            tmpodd =  max(sumeven-nums[i],sumodd)
+            sumeven ,sumodd = tmpeven , tmpodd
+        return sumeven
+    
+    
+    
+        dp = {}
+        def dfs(i,even):
+            if i == len(nums):
+                return 0 
+            if (i,even) in dp:
+                return dp[(i,even)]
+            total = nums[i] if even else -1*nums[i]
+            dp[(i,even)] = max(total+dfs(i+1,not even),dfs(i+1,even))
+            return dp[(i,even)]
+        return dfs(0,True)
