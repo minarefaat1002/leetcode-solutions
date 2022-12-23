7b2dd1a4044b7db69f39edda04f96a1f3bfd3bbd
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-12-23 19:07:03 +0200

    Time: 67 ms (60.77%), Space: 14.4 MB (27.10%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.py
@@ -0,0 +1,28 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return None
+        newNode = Node(node.val,None)
+        hashMap = {}
+        visited = [False]*101
+        hashMap[newNode.val] = newNode
+        def dfs(newNode,node):
+            if visited[node.val]:
+                return
+            visited[node.val] = True
+            for neighbor in node.neighbors:
+                ne = Node(neighbor.val,None) if neighbor.val not in hashMap else hashMap[neighbor.val]
+                hashMap[ne.val] = ne
+                newNode.neighbors.append(ne)
+                dfs(ne,neighbor)
+            return newNode
+        dfs(newNode,node)
+        return newNode    
