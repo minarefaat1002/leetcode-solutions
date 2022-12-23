0fad9ed5123564c147c9e07f7b9f28d8d3bc027e
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-12-24 00:41:39 +0200

    Time: 1497 ms (89.41%), Space: 30.7 MB (5.71%) - LeetHub

---
+++ 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        res = []
+        l,r = 0 , len(nums)-1
+        while l<=r:
+            res.append(nums[l])
+            l+=1
+            if l<r:
+                res.append(nums[r])
+                r-=1
+        return res 
