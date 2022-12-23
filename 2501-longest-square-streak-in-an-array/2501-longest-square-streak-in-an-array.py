898507ca67fb1625ec883a8f744f4760950871f7
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-12-23 23:49:33 +0200

    Time: 1002 ms (85.12%), Space: 31.8 MB (55.19%) - LeetHub

--- 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.py
+++ 2501-longest-square-streak-in-an-array/2501-longest-square-streak-in-an-array.py
@@ -1,13 +1,11 @@
 class Solution:
     def longestSquareStreak(self, nums: List[int]) -> int:
-        nums.sort()
         hashMap = {}
-        for i in range(len(nums)):
-            if sqrt(nums[i]) not in hashMap:
-                hashMap[nums[i]] = 1
-            else:
-                hashMap[nums[i]] = hashMap[sqrt(nums[i])] +1
+        nums.sort()
+        for num in nums:
+            hashMap[num] = hashMap.get(sqrt(num),0) + 1
         Max = -1
         for item in hashMap:
-            Max = max(Max,hashMap[item])
-        return Max if Max !=1 else -1
+            if hashMap[item] > Max:
+                Max = hashMap[item]
+        return Max if Max > 1 else -1
