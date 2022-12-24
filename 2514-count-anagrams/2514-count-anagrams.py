d351c9c7ec8ac9f209c77223a4cb7547daac9e77
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-12-24 19:44:54 +0200

    Time: 2042 ms (9.09%), Space: 15.6 MB (100.00%) - LeetHub

--- 2514-count-anagrams/2514-count-anagrams.py
+++ 2514-count-anagrams/2514-count-anagrams.py
@@ -1,26 +1,18 @@
 class Solution:
-    def factorial(self,num):
-        MOD = 10**9 + 7
-        if num <=1:
-            return 1
-        return num*factorial(num-1)
     def countAnagrams(self, s: str) -> int:
-        MOD = 10**9 + 7
-        words = s.split(" ")
-        anagrams = 1
-        for word in words:
-            freq = [0]*26
-            letters = 0
-            for char in word:
-                freq[ord(char)-ord('a')] += 1
-                letters += 1
-            anagramsOfCurrentWord = self.factorial(letters)
-            denominator = 1
-            for f in freq:
-                denominator *= self.factorial(f)
-            anagramsOfCurrentWord = anagramsOfCurrentWord//denominator
-            anagrams = anagrams*anagramsOfCurrentWord
-        return anagrams%MOD
-            
+
+        def get(word):
+            n = math.factorial(len(word))
+
+            for v in Counter(word).values():
+                n //= math.factorial(v)
+
+            return int(n) % (10**9 + 7)
+
+        total = 1  
+        for word in s.split():
+            total *= get(word)
+
+        return total % (10**9 + 7)
             
             
