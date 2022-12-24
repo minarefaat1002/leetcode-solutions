1aac1a12d0d28bda539adeaf850c5ba8e80fdf87
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-12-24 13:02:56 +0200

    Time: 47 ms (76.03%), Space: 14.1 MB (47.78%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
@@ -13,7 +13,7 @@ class Solution:
         dp = [[1,0] for _ in range(n+1)]
         dp[1][0] = 1
         for i in range(2,n+1):
-            for j in range(3):
+            for j in range(2):
                 dp[i][0] = dp[i-1][0] + dp[i-2][0] + 2*dp[i-1][1]
                 dp[i][1] = dp[i-2][0] + dp[i-1][1]
         return dp[-1][0]%MOD
