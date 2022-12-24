df82804067d082eff5ce19edc541c770f841333a
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-12-24 19:44:28 +0200

    Time: 1999 ms (9.09%), Space: 15.7 MB (100.00%) - LeetHub

---
+++ 2514-count-anagrams/2514-count-anagrams.py
@@ -0,0 +1,26 @@
+class Solution:
+    def factorial(self,num):
+        MOD = 10**9 + 7
+        if num <=1:
+            return 1
+        return num*factorial(num-1)
+    def countAnagrams(self, s: str) -> int:
+        MOD = 10**9 + 7
+        words = s.split(" ")
+        anagrams = 1
+        for word in words:
+            freq = [0]*26
+            letters = 0
+            for char in word:
+                freq[ord(char)-ord('a')] += 1
+                letters += 1
+            anagramsOfCurrentWord = self.factorial(letters)
+            denominator = 1
+            for f in freq:
+                denominator *= self.factorial(f)
+            anagramsOfCurrentWord = anagramsOfCurrentWord//denominator
+            anagrams = anagrams*anagramsOfCurrentWord
+        return int(anagrams)%MOD
+            
+            
+            
