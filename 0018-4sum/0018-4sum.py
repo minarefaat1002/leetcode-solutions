fe27e809a9a729589a613f5de31c2f0ab851b0da
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-12-29 19:01:04 +0200

    Time: 1748 ms (48.53%), Space: 13.9 MB (94.15%) - LeetHub

---
+++ 0018-4sum/0018-4sum.py
@@ -0,0 +1,39 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        # hashMap = collections.defaultdict(list)
+        # res = set()
+        # nums.sort()
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         hashMap[nums[i]+nums[j]].append((i,j))
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         rightSide = target - nums[i] - nums[j]
+        #         z = 0
+        #         while z < len(hashMap[rightSide]):
+        #             if rightSide in hashMap and hashMap[rightSide][z][0] not in [i,j] and hashMap[rightSide][z][1] not in [i,j]:
+        #                 comb = [nums[i],nums[j],nums[hashMap[rightSide][z][0]],nums[hashMap[rightSide][z][1]]]
+        #                 comb.sort()
+        #                 res.add(tuple(comb))
+        #             z+=1
+        # return list(res) 
+        res = set()
+        nums.sort()
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if j>i+1 and nums[j] == nums[j-1]:
+                    continue 
+                l = j+1
+                r = len(nums)-1
+                while l < r:
+                    if nums[l] + nums[r] == target - nums[i] - nums[j]:
+                        comb = [nums[l],nums[r],nums[i],nums[j]]
+                        comb.sort()
+                        res.add(tuple(comb))
+                        l+=1
+                        r-=1
+                    elif nums[l] + nums[r] > target - nums[i] - nums[j]:
+                        r-=1
+                    else:
+                        l+=1
+        return list(res)
