40a6e1404867324a53256745d054de28487c9dec
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-12-30 18:31:13 +0200

    Time: 880 ms (76.90%), Space: 27.9 MB (62.25%) - LeetHub

---
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        costs.sort()
+        count = 0
+        for cost in costs:
+            coins -= cost
+            if coins < 0:
+                return count 
+            count +=1
+            
+        return count 
