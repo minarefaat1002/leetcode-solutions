9492755a4167daece24e33cbf725203be1ca6016
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-12-30 13:47:10 +0200

    Time: 1371 ms (85.06%), Space: 24.4 MB (97.70%) - LeetHub

---
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maximumScore(self, nums: List[int], k: int) -> int:
+        nums.append(0)
+        nums.insert(0,0)
+        stack = []
+        ans = 0
+        for i in range(len(nums)):
+            while stack and nums[i] < nums[stack[-1]]:
+                h = nums[stack.pop()]
+                w = i - stack[-1] -1
+                if k+1 <= i-1 and k+1 >= stack[-1]+1:
+                    ans = max(ans, h * w)
+            stack.append(i)
+        nums.pop()
+        return ans
