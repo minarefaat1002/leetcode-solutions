eee3e9fe621b4996db1a0742861f66480efb021d
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2022-12-30 16:53:10 +0200

    Time: 1937 ms (71.18%), Space: 21.7 MB (82.97%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -0,0 +1,34 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def mergeSort(arr):
+            if len(arr) < 2:
+                return 
+            mid = len(arr)//2
+            arr1 = arr[:mid]
+            arr2 = arr[mid:]
+            mergeSort(arr1)
+            mergeSort(arr2)
+            i = 0
+            k = 0
+            z = 0
+            while k < len(arr1) and z < len(arr2):
+                if arr1[k] < arr2[z]:
+                    arr[i] = arr1[k]
+                    k+=1
+                    i+=1
+                else:
+                    arr[i] = arr2[z]
+                    z+=1
+                    i+=1
+            while k < len(arr1):
+                arr[i] = arr1[k]
+                k+=1
+                i+=1
+            while z < len(arr2):
+                arr[i] = arr2[z]
+                z+=1
+                i+=1
+        mergeSort(nums)
+        return nums
+                
+            
