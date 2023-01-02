68d8e619fd38bcaabfa8f1fc852255b9cf5d3a04
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-01-02 15:06:35 +0200

    Time: 2065 ms (83.81%), Space: 31.1 MB (91.26%) - LeetHub

---
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.py
@@ -0,0 +1,35 @@
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+        self.N = len(nums)
+        self.tree = [0] + nums.copy()
+        for i in range(1,self.N+1):
+            j = i + self.LSB(i)
+            if j <= self.N:
+                self.tree[j] += self.tree[i]
+    def LSB(self,num):
+        return num & (-num)
+    
+    def update(self, index: int, val: int) -> None:
+        i = index+1
+        val = val - self.sumRange(i-1,i-1)
+        while i <= self.N:
+            self.tree[i] = self.tree[i] + val
+            i = i + self.LSB(i)
+    def prefixSum(self,i):
+        i = i + 1
+        Sum = 0
+        while i > 0:
+            Sum = Sum + self.tree[i]
+            i = i - self.LSB(i)
+        return Sum
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.prefixSum(right) - self.prefixSum(left-1)
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# obj.update(index,val)
+# param_2 = obj.sumRange(left,right)
