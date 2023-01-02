b02f1d18368050ad423e99d12facb6e603902c15
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-01-02 18:01:20 +0200

    Time: 3559 ms (80.43%), Space: 42.2 MB (25.69%) - LeetHub

---
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
@@ -0,0 +1,37 @@
+class Solution:
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        counts = [0] * len(nums)
+        indexed_nums = [(nums[key_index], key_index) for key_index in range(len(nums))]
+        def mergeSort(arr):
+            if len(arr) < 2:
+                return 
+            mid = len(arr)//2
+            arr1 = arr[:mid]
+            mergeSort(arr1)
+            arr2 = arr[mid:]
+            mergeSort(arr2)
+            i = 0
+            k = 0
+            z = 0
+            while k < len(arr1) and z < len(arr2):
+                if arr1[k][0] > arr2[z][0]:
+                    counts[arr1[k][1]] += len(arr2)-z
+                    arr[i] = arr1[k]
+                    k+=1
+                    i+=1
+                else:
+                    arr[i] = arr2[z]
+                    z+=1
+                    i+=1
+            while k < len(arr1):
+                arr[i] = arr1[k]
+                k+=1
+                i+=1
+            while z < len(arr2):
+                arr[i] = arr2[z]
+                z+=1
+                i+=1
+        mergeSort(indexed_nums)
+        return counts
+                
+            
