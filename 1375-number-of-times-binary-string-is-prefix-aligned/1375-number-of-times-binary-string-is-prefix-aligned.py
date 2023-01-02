6ac916dcfbe86cdb74cc374cb5aa4f8e37240510
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-01-02 21:58:54 +0200

    Time: 1149 ms (35.92%), Space: 20.9 MB (66.94%) - LeetHub

--- 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.py
+++ 1375-number-of-times-binary-string-is-prefix-aligned/1375-number-of-times-binary-string-is-prefix-aligned.py
@@ -2,10 +2,8 @@ class Solution:
     def numTimesAllBlue(self, flips: List[int]) -> int:
         Sum = 0
         count = 0
-        n = 0
         for i in range(len(flips)):
             Sum += flips[i]
-            n = n + i + 1
-            if Sum == n :
+            if Sum == (i+1)*(i+2)//2 :
                 count +=1
         return count
