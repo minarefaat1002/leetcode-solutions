bc8cf355bd5e737f468124a30b2924a07a2f9664
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-01-04 14:24:15 +0200

    Time: 258 ms (7.88%), Space: 14.2 MB (60.58%) - LeetHub

---
+++ 1366-rank-teams-by-votes/1366-rank-teams-by-votes.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rankTeams(self, votes: List[str]) -> str:
+        items = [[0]*26 for _ in range(26)]
+        for vote in votes:
+            for i in range(len(vote)):
+                items[ord(vote[i]) - ord('A')][i] += 1
+        for i in range(26):
+            items[i].append(25-i)
+            items[i].append(chr(ord('A')+i))
+        items.sort(reverse = True)
+        res = ""
+        for item in items:
+            res += item[-1] if item[-1] in votes[0] else ""
+        return res
