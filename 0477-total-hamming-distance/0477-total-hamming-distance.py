c48dbe165b8303d714609efa4888c6348f5e2b45
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-01-06 21:47:56 +0200

    Time: 717 ms (65.18%), Space: 15.5 MB (25.93%) - LeetHub

--- 0477-total-hamming-distance/0477-total-hamming-distance.py
+++ 0477-total-hamming-distance/0477-total-hamming-distance.py
@@ -2,10 +2,12 @@ class Solution:
     def totalHammingDistance(self, nums: List[int]) -> int:
         setBits = [0]*32
         for num in nums:
-            for i in range(32):
+            i = 0
+            while i < 32 and num:
                 mask = 1
                 setBits[i] += mask&num
                 num>>=1
+                i+=1
         count = 0
         for bit in setBits:
             count += (len(nums)-bit)*bit
