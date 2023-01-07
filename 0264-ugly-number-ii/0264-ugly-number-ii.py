56e7aef39f18cfeb684bcf04841bdd729eaf3c5e
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-01-07 03:04:44 +0200

    Time: 124 ms (93.29%), Space: 13.8 MB (84.45%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.py
+++ 0264-ugly-number-ii/0264-ugly-number-ii.py
@@ -1,16 +1,31 @@
 class Solution:
     def nthUglyNumber(self, n: int) -> int:
-        heap = [2,3,5]
-        last = 1
-        heapq.heapify(heap)
-        n-=1
-        while n:
-            popped = heapq.heappop(heap)
-            if popped == last:
-                continue
-            last = popped
-            heapq.heappush(heap,popped*2)
-            heapq.heappush(heap,popped*3)
-            heapq.heappush(heap,popped*5)
-            n-=1
-        return last
+        # heap = [2,3,5]
+        # last = 1
+        # heapq.heapify(heap)
+        # n-=1
+        # while n:
+        #     popped = heapq.heappop(heap)
+        #     if popped == last:
+        #         continue
+        #     last = popped
+        #     heapq.heappush(heap,popped*2)
+        #     heapq.heappush(heap,popped*3)
+        #     heapq.heappush(heap,popped*5)
+        #     n-=1
+        # return last
+        dp = [0]*n
+        dp[0] = 1
+        i,j,z = 0,0,0
+        for r in range(1,n):
+            twoMultiple = dp[i]*2
+            threeMultiple = dp[j]*3
+            fiveMultiple = dp[z]*5
+            dp[r] = min(twoMultiple,threeMultiple,fiveMultiple)
+            if dp[r] == twoMultiple:
+                i+=1
+            if dp[r] == threeMultiple:
+                j+=1
+            if dp[r] == fiveMultiple:
+                z+=1
+        return dp[n-1]
