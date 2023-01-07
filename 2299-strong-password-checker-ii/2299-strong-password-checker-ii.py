2e9222030d91dfab654e6c51d04f3f958ed6e0e4
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-01-07 04:57:34 +0200

    Time: 31 ms (89.96%), Space: 13.8 MB (98.21%) - LeetHub

---
+++ 2299-strong-password-checker-ii/2299-strong-password-checker-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def strongPasswordCheckerII(self, password: str) -> bool:
+        lowerCase = False
+        upperCase = False
+        digit = False
+        specialChar = False
+        for i in range(len(password)):
+            prevChar = password[i-1] if i > 0 else ""
+            char = password[i]
+            if ord(char)-ord('a') <=25 and ord(char)-ord('a') >=0:
+                lowerCase = True
+            elif ord(char)-ord('A') <=25 and ord(char)-ord('A') >=0:
+                upperCase = True
+            elif ord(char) - ord('0') <= 9 and ord(char) - ord('0') >= 0:
+                digit = True
+            elif char in "!@#$%^&*()-+":
+                specialChar = True
+            if i > 0 and char == prevChar:
+                return False
+        return len(password) >= 8 and lowerCase and upperCase and digit and specialChar 
