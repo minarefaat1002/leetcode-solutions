1b69a16fd3b82c81495caa5ecb6736ccb4b344c9
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-01-10 18:44:16 +0200

    Time: 44 ms (98.69%), Space: 14.6 MB (85.21%) - LeetHub

---
+++ 1325-delete-leaves-with-a-given-value/1325-delete-leaves-with-a-given-value.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
+        def dfs(root):
+            if not root:
+                return
+            if dfs(root.left):
+                root.left = None
+            if dfs(root.right):
+                root.right = None
+            if root.val == target and not root.left and not root.right:
+                return True
+            return False
+        dfs(root)
+        if root.val == target and not root.left and not root.right:
+            return None
+        return root
