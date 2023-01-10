6fabaa224149e4bb31bce675ac34b13c44ee5bbc
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-01-10 18:17:09 +0200

    Time: 160 ms (99.33%), Space: 20.5 MB (37.90%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -7,20 +7,32 @@ class Solution:
         # return False
         
         
-        def exists(row,target):
-            l = 0 
-            r = len(row) - 1
-            while l <= r:
-                mid = (l+r)//2
-                if row[mid] == target:
-                    return True
-                elif row[mid] < target:
-                    l = mid + 1
-                else:
-                    r = mid - 1
-            return False
-        for i in range(len(matrix)):
-            row = matrix[i]
-            if exists(row,target):
+        # def exists(row,target):
+        #     l = 0 
+        #     r = len(row) - 1
+        #     while l <= r:
+        #         mid = (l+r)//2
+        #         if row[mid] == target:
+        #             return True
+        #         elif row[mid] < target:
+        #             l = mid + 1
+        #         else:
+        #             r = mid - 1
+        #     return False
+        # for i in range(len(matrix)):
+        #     row = matrix[i]
+        #     if exists(row,target):
+        #         return True
+        # return False
+        r = 0
+        c = len(matrix[0]) - 1
+        while r < len(matrix) and c>-1:
+            row = matrix
+            lastCell = matrix[r][c]
+            if lastCell == target:
                 return True
+            elif target > lastCell:
+                r += 1
+            else:
+                c-=1
         return False
