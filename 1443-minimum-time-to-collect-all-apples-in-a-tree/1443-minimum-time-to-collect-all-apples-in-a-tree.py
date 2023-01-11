b564c2d3db8c43113799b3f4c7fd20d74ef50c96
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-01-11 15:43:57 +0200

    Time: 654 ms (96.79%), Space: 50.9 MB (64.10%) - LeetHub

--- 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.py
@@ -1,23 +1,50 @@
 class Solution:
     def minTime(self, n: int, edges: List[List[int]], hasApple: List[bool]) -> int:
+        # graph = {}
+        # Sum = [0]
+        # graph[edges[0][0]] = [edges[0][1]]
+        # used = [False]*n
+        # used[edges[0][0]] = True
+        # used[edges[0][1]] = True
+        # for edge in edges[1:]:
+        #     if used[edge[0]]:
+        #         if edge[0] not in graph:
+        #             graph[edge[0]] = []
+        #         graph[edge[0]].append(edge[1])
+        #         used[edge[1]] = True
+        #     elif used[edge[1]]:
+        #         if edge[1] not in graph:
+        #             graph[edge[1]] = []
+        #         graph[edge[1]].append(edge[0])
+        #         used[edge[0]] = True
+        # def dfs(vertex):
+        #     if vertex not in graph:
+        #         if hasApple[vertex]:
+        #             return True
+        #         return False
+        #     hasAple = False
+        #     for child in graph[vertex]:
+        #         df = dfs(child)
+        #         if df:
+        #             Sum[0]+=2
+        #         hasAple = hasAple or df
+        #     return hasAple or hasApple[vertex]
+        # dfs(0)
+        # return Sum[0]
         graph = {}
         Sum = [0]
-        graph[edges[0][0]] = [edges[0][1]]
-        used = [False]*n
-        used[edges[0][0]] = True
-        used[edges[0][1]] = True
-        for edge in edges[1:]:
-            if used[edge[0]]:
-                if edge[0] not in graph:
-                    graph[edge[0]] = []
-                graph[edge[0]].append(edge[1])
-                used[edge[1]] = True
-            elif used[edge[1]]:
-                if edge[1] not in graph:
-                    graph[edge[1]] = []
-                graph[edge[1]].append(edge[0])
-                used[edge[0]] = True
+        for edge in edges:
+            if edge[0] not in graph:
+                graph[edge[0]] = []
+            if edge[1] not in graph:
+                graph[edge[1]] = []
+            graph[edge[0]].append(edge[1])
+            graph[edge[1]].append(edge[0])
+        visited = [False]*n
         def dfs(vertex):
+            if visited[vertex]:
+                return False
+            visited[vertex] = True
             if vertex not in graph:
                 if hasApple[vertex]:
                     return True
@@ -26,7 +53,7 @@ class Solution:
             for child in graph[vertex]:
                 df = dfs(child)
                 if df:
-                    Sum[0]+=2
+                    Sum[0] += 2
                 hasAple = hasAple or df
             return hasAple or hasApple[vertex]
         dfs(0)
