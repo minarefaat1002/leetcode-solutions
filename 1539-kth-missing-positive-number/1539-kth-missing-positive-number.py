bd19d198c7730511d8495a917f5c2f88adc31052
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-01-11 23:26:48 +0200

    Time: 47 ms (96.12%), Space: 14 MB (57.12%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
@@ -1,7 +1,11 @@
 class Solution:
     def findKthPositive(self, arr: List[int], k: int) -> int:
+        if k < arr[0]:
+            return k
+        if k > arr[-1] - len(arr):
+            return k + len(arr)
         l = 0
-        r = len(arr)
+        r = len(arr) - 1
         res = -1
         while l < r:
             mid = (l+r)//2
@@ -9,4 +13,4 @@ class Solution:
                 r = mid 
             else:
                 l = mid + 1
-        return k+l
+        return k-(arr[l-1] - (l)) + arr[l-1]
