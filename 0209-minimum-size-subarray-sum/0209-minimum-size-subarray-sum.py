8ae401d495ab0a8683dbbef54c1a6fb864006a54
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-01-13 02:44:13 +0200

    Time: 259 ms (90.30%), Space: 27.2 MB (42.44%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -2,11 +2,11 @@ class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         l = 0
         Sum = 0
-        shortest = float('inf')
+        shortest = 10**5 + 1
         for r in range(len(nums)):
             Sum += nums[r]
             while Sum >= target:
                 Sum -= nums[l]
                 shortest = min(shortest,r-l+1)
                 l += 1
-        return shortest if shortest != float('inf') else 0
+        return shortest if shortest != 100001 else 0
