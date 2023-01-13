e68f66859172e2b51da49330c100708e78a10615
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-01-13 21:44:08 +0200

    Time: 36 ms (85.95%), Space: 13.8 MB (99.28%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.py
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.py
@@ -1,11 +1,25 @@
 class Solution:
     def wiggleMaxLength(self, nums: List[int]) -> int:
+        # this solution is similar to longest increasing subsequence problem
+        # pos = [1]*len(nums)
+        # neg = [1]*len(nums)
+        # for i in range(1,len(nums)):
+        #     for j in range(i):
+        #         if nums[i] > nums[j]:
+        #             pos[i] = max(pos[i],neg[j] + 1)
+        #         elif nums[i] < nums[j]:
+        #             neg[i] = max(neg[i],pos[j] + 1)
+        # return max(max(pos),max(neg))
         pos = [1]*len(nums)
         neg = [1]*len(nums)
         for i in range(1,len(nums)):
-            for j in range(i):
-                if nums[i] > nums[j]:
-                    pos[i] = max(pos[i],neg[j] + 1)
-                elif nums[i] < nums[j]:
-                    neg[i] = max(neg[i],pos[j] + 1)
+            if nums[i] > nums[i-1]:
+                pos[i] = neg[i-1] + 1
+                neg[i] = neg[i-1]
+            elif nums[i] < nums[i-1]:
+                neg[i] = pos[i-1] + 1
+                pos[i] = pos[i-1]
+            else:
+                neg[i] = neg[i-1]
+                pos[i] = pos[i-1]
         return max(max(pos),max(neg))
