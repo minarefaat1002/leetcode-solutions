1f51ab2a95f9a4502b022e4fa0eb455029718370
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-01-14 17:28:35 +0200

    Time: 42 ms (85.63%), Space: 13.9 MB (56.90%) - LeetHub

---
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
@@ -0,0 +1,38 @@
+class Solution:
+    def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
+        parent = [i for i in range(26)]
+        rank = [1]*26
+        def find(n):
+            p = parent[n]
+            while p != parent[p]:
+                parent[p] = parent[parent[p]]
+                p = parent[p]
+            return p
+        def union(n1,n2):
+            p1,p2 = find(n1),find(n2)
+            if p1 == p2:
+                return False
+            if rank[p1] > rank[p2]:
+                parent[p2] = p1
+                rank[p1] += rank[p2]
+            else:
+                parent[p1] = p2
+                rank[p2] += rank[p1]
+            return True
+        for c1,c2 in zip(s1,s2):
+            union(ord(c1)-ord('a'),ord(c2)-ord('a'))
+        hashMap = {}
+        for i in range(26):
+            if find(i) not in hashMap:
+                hashMap[find(i)] = [i]
+            else:
+                hashMap[find(i)].append(i)
+        for item in hashMap:
+            Min = item
+            for char in hashMap[item]:
+                Min = Min if Min < char else char
+            hashMap[item].append(Min)
+        s = ""
+        for char in baseStr:
+            s += chr(hashMap[find(ord(char)-ord('a'))][-1]+ord('a'))
+        return s
