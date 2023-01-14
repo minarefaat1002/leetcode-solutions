bd3de4b15b321cf17f57d99d468ff89e242875de
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-01-14 17:30:26 +0200

    Time: 39 ms (93.68%), Space: 14.1 MB (21.84%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
@@ -10,8 +10,6 @@ class Solution:
             return p
         def union(n1,n2):
             p1,p2 = find(n1),find(n2)
-            if p1 == p2:
-                return False
             if rank[p1] > rank[p2]:
                 parent[p2] = p1
                 rank[p1] += rank[p2]
