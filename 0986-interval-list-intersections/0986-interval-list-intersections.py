1813fa25af0a1908873701d3162aa3d5074b87e7
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-01-16 02:38:19 +0200

    Time: 172 ms (52.23%), Space: 14.7 MB (83.68%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.py
+++ 0986-interval-list-intersections/0986-interval-list-intersections.py
@@ -8,9 +8,7 @@ class Solution:
         while i < len(firstList) and j < len(secondList):
             a = firstList[i]
             b = secondList[j]
-            if a[1] >= b[0] and a[1] <= b[1]:
-                intersection.append([max(a[0],b[0]),min(a[1],b[1])])
-            elif b[1] >= a[0] and b[1] <= a[1]:
+            if min(a[1],b[1]) >= max(a[0],b[0]):
                 intersection.append([max(a[0],b[0]),min(a[1],b[1])])
             j += 1 if a[1] >= b[1] else 0 
             i += 1 if b[1] >= a[1] else 0
