305d1c643a93cd0cb88e99bbf3a3f47a500c3cfd
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-01-16 02:36:21 +0200

    Time: 158 ms (72.28%), Space: 14.8 MB (83.68%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.py
+++ 0986-interval-list-intersections/0986-interval-list-intersections.py
@@ -1,7 +1,5 @@
 class Solution:
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
-        if not(firstList and secondList):
-            return []
         firstList.sort()
         secondList.sort()
         intersection = []
