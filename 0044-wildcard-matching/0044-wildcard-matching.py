12ae21f3286b2c15b908b16465889b28677c33e8
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-01-16 19:53:19 +0200

    Time: 1528 ms (26.07%), Space: 93.6 MB (25.34%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -1,22 +1,42 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        newP = ""
-        for i,char in enumerate(p):
-            if len(newP) !=0 and char =="*" and char == newP[-1]:
-                continue
-            newP +=char
+        # newP = ""
+        # for i,char in enumerate(p):
+        #     if len(newP) !=0 and char =="*" and char == newP[-1]:
+        #         continue
+        #     newP +=char
+        # dp = {}
+        # def dfs(i,j):
+        #     if i == len(s) and j==len(newP):
+        #         return True
+        #     if j == len(newP):
+        #         return False
+        #     if (i,j) in dp:
+        #         return dp[(i,j)]
+        #     if i<len(s) and s[i] == newP[j] or newP[j] == "?":
+        #         dp[(i,j)] = dfs(i+1,j+1)
+        #         return dp[(i,j)]
+        #     if newP[j] == "*":
+        #         res = False
+        #         for z in range(i,len(s)+1):
+        #             res = res or dfs(z,j+1)
+        #         dp[(i,j)] = res
+        #         return dp[(i,j)]
+        # return dfs(0,0)
         dp = {}
         def dfs(i,j):
-            if i == len(s) and j==len(newP):
+            if i == len(s) and j==len(p):
                 return True
-            if j == len(newP):
+            if j == len(p):
                 return False
             if (i,j) in dp:
                 return dp[(i,j)]
-            if i<len(s) and s[i] == newP[j] or newP[j] == "?":
+            if i<len(s) and s[i] == p[j] or p[j] == "?":
                 dp[(i,j)] = dfs(i+1,j+1)
                 return dp[(i,j)]
-            if newP[j] == "*":
+            if j<len(p)-1 and p[j] == "*" and p[j+1] == "*":
+                return dfs(i,j+1)
+            if p[j] == "*":
                 res = False
                 for z in range(i,len(s)+1):
                     res = res or dfs(z,j+1)
