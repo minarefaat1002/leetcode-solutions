15e519671fc1b9572dcd8dbbf13ad85aed9cf93c
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-01-18 02:52:04 +0200

    Time: 212 ms (57.00%), Space: 17.6 MB (64.34%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -12,16 +12,28 @@ class Solution:
                 j+=d[1]
             i-=d[0]
             j-=d[1]
-            if i == 0 and j!=len(mat[0])-1:
+            if i == len(mat)-1:
                 j+=1
-                d = [1,-1]
-            elif j == 0 and i != len(mat)-1:
+                d=[-1,1]
+            elif j == len(mat[0])-1:
                 i+=1
-                d = [-1,1]
-            elif j == len(mat[0]) - 1:
-                i+=1
-                d = [1,-1]
-            elif i == len(mat) - 1:
+                d=[1,-1]
+            elif i == 0:
                 j+=1
-                d = [-1,1]
+                d=[1,-1]
+            elif j == 0:
+                i+=1
+                d=[-1,1]
+            # if i == 0 and j!=len(mat[0])-1:
+            #     j+=1
+            #     d = [1,-1]
+            # elif j == 0 and i != len(mat)-1:
+            #     i+=1
+            #     d = [-1,1]
+            # elif j == len(mat[0]) - 1:
+            #     i+=1
+            #     d = [1,-1]
+            # elif i == len(mat) - 1:
+            #     j+=1
+            #     d = [-1,1]
         return res
