2322451bb32c2cfb6854436990fce7e05a141c6b
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-01-19 04:19:21 +0200

    Time: 139 ms (69.42%), Space: 15.8 MB (6.48%) - LeetHub

---
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -0,0 +1,22 @@
+class Solution:
+    def closedIsland(self, grid: List[List[int]]) -> int:
+        count = 0
+        visited = set()
+        def dfs(i,j):
+            if i < 0 or j < 0 or i == len(grid) or j == len(grid[0]):
+                return False
+            if grid[i][j] == 1:
+                return True
+            if (i,j) in visited:
+                return True
+            visited.add((i,j))
+            d1 = dfs(i+1,j) 
+            d2 = dfs(i,j+1) 
+            d3 = dfs(i-1,j) 
+            d4 = dfs(i,j-1)
+            return d1 and d2 and d3 and d4
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 0 and (i,j) not in visited and dfs(i,j):
+                    count += 1
+        return count 
