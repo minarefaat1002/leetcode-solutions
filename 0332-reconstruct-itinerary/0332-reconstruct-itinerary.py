55df8fdb81a2c75ac2f673a9da3ed58603e151a4
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-01-19 03:22:06 +0200

    Time: 87 ms (73.52%), Space: 14.6 MB (53.65%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        tickets.sort()
+        graph = {}
+        for ticket in tickets:
+            if ticket[0] not in graph:
+                graph[ticket[0]] = []
+            graph[ticket[0]].append(ticket[1])
+        res = ["JFK"]
+        def dfs(cur):
+            if len(res) == len(tickets) + 1:
+                return True
+            if cur not in graph or len(cur) == 0:
+                return False
+            temp = graph[cur].copy()
+            for i in range(len(temp)):
+                d = temp[i]
+                graph[cur].pop(i)
+                res.append(d)
+                if dfs(d):
+                    return True
+                graph[cur].insert(i,d)
+                res.pop()
+            return False
+        dfs("JFK")
+        return res
