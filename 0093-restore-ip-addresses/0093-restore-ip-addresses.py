f1762ea309b871d88eb7100dad19dc816cb80d25
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-01-21 14:33:21 +0200

    Time: 50 ms (51.09%), Space: 13.9 MB (79.81%) - LeetHub

---
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -0,0 +1,23 @@
+class Solution:
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        res = []
+        def dfs(i,temp):
+            if i == len(s) and len(temp) == 4:
+                res.append(".".join(temp.copy()))
+            if i == len(s):
+                return
+            temp.append(s[i])
+            dfs(i+1,temp)
+            temp.pop()
+            if s[i] == "0":
+                return
+            if i+1 < len(s) :
+                temp.append(s[i:i+2])
+                dfs(i+2,temp)
+                temp.pop()
+            if i + 2 < len(s) and int(s[i:i+3]) <= 255:
+                temp.append(s[i:i+3])
+                dfs(i+3,temp)
+                temp.pop()
+        dfs(0,[])
+        return res
