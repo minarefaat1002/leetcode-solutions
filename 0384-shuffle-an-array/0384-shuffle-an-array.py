0f52f144644d6043e42e60e10e5b5329ecdb23ff
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-01-26 16:22:16 +0200

    Time: 305 ms (28.72%), Space: 17.7 MB (15.78%) - LeetHub

--- 0384-shuffle-an-array/0384-shuffle-an-array.py
+++ 0384-shuffle-an-array/0384-shuffle-an-array.py
@@ -1,3 +1,38 @@
+'''
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+        self.original = nums.copy()
+
+    def reset(self) -> List[int]:
+        self.nums = self.original.copy()
+        return self.nums
+    def shuffle(self) -> List[int]:
+        shuffled = []
+        while len(self.nums) > 0:
+            rand_index = random.randrange(0,len(self.nums))
+            shuffled.append(self.nums[rand_index])
+            self.nums.pop(rand_index)
+        self.nums = shuffled
+        return self.nums
+'''
+'''
+class Solution:
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+        self.original = nums.copy()
+
+    def reset(self) -> List[int]:
+        self.nums = self.original.copy()
+        return self.nums
+    def shuffle(self) -> List[int]:
+        rand_values = [random.random() for i in range(len(self.nums))]
+        rand_indexes = [i for i in range(len(self.nums))]
+        rand_indexes.sort(key=lambda i:rand_values[i])
+        self.nums = [self.nums[i] for i in rand_indexes]
+        return self.nums 
+'''
 class Solution:
     def __init__(self, nums: List[int]):
         self.nums = nums
