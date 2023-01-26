5d4deb1f559bd767b19df27eee3d7038142d8296
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-01-26 16:20:42 +0200

    Time: 335 ms (26.67%), Space: 17.6 MB (63.11%) - LeetHub

--- 0384-shuffle-an-array/0384-shuffle-an-array.py
+++ 0384-shuffle-an-array/0384-shuffle-an-array.py
@@ -7,11 +7,15 @@ class Solution:
         self.nums = self.original.copy()
         return self.nums
     def shuffle(self) -> List[int]:
-        rand_values = [random.random() for i in range(len(self.nums))]
-        rand_indexes = [i for i in range(len(self.nums))]
-        rand_indexes.sort(key=lambda i:rand_values[i])
-        self.nums = [self.nums[i] for i in rand_indexes]
-        return self.nums 
+        last_index = len(self.nums)-1
+        while last_index:
+            rand_index = random.randint(0,last_index)
+            temp = self.nums[last_index]
+            self.nums[last_index] = self.nums[rand_index]
+            self.nums[rand_index] = temp
+            last_index-=1
+        return self.nums
+        
 
 
 # Your Solution object will be instantiated and called as such:
