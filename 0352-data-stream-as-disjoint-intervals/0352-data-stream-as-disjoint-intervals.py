77c54a1b2791f678b40692c8e72f800d05cc6d2f
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-01-28 14:35:52 +0200

    Time: 929 ms (15.06%), Space: 19.4 MB (8.36%) - LeetHub

---
+++ 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.py
@@ -0,0 +1,23 @@
+from sortedcontainers import SortedDict
+class SummaryRanges:
+
+    def __init__(self):
+        self.sd = SortedDict()
+
+    def addNum(self, value: int) -> None:
+        self.sd[value] = 4
+
+    def getIntervals(self) -> List[List[int]]:
+        res = []
+        for key in self.sd:
+            if res and key-1 == res[-1][1]:
+                res[-1][1] += 1
+            else:
+                res.append([key,key])
+        return res
+
+
+# Your SummaryRanges object will be instantiated and called as such:
+# obj = SummaryRanges()
+# obj.addNum(value)
+# param_2 = obj.getIntervals()
