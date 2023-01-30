c31d8b8bcbbabb3a699e05f72adf5d5013622c4e
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-01-31 00:39:05 +0200

    Time: 4865 ms (12.36%), Space: 81.3 MB (18.46%) - LeetHub

---
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.py
@@ -0,0 +1,21 @@
+class Solution:
+    def canMeasureWater(self, jug1Capacity: int, jug2Capacity: int, targetCapacity: int) -> bool:
+        q = deque()
+        q.append([0,0])
+        visited = set()
+        while q:
+            c1,c2 = q.popleft()
+            if (c1,c2) in visited:
+                continue
+            if c1+c2 == targetCapacity:
+                return True
+            visited.add((c1,c2))
+            q.append([jug1Capacity,c2])
+            q.append([c1,jug2Capacity])
+            q.append([jug1Capacity,jug2Capacity])
+            q.append([0,c2])
+            q.append([c1,0])
+            q.append([c1+min(jug1Capacity-c1,c2),max(c2-(jug1Capacity-c1),0)])
+            c1,c2 = c2,c1
+            q.append([c1+min(jug1Capacity-c1,c2),max(c2-(jug1Capacity-c1),0)])
+        return False
