b2cb1de69624a837cd476c1935c60a8673c23ae0
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-02-03 02:01:25 +0200

    Time: 203 ms (70.86%), Space: 14.3 MB (79.18%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
@@ -1,20 +1,16 @@
 class Solution:
     def findBall(self, grid: List[List[int]]) -> List[int]:
-        
-        def dfs(i, j, grid):
-            if i == len(grid):
-                return j
-            if grid[i][j] == 1:
-                if j >= len(grid[0]) - 1 or grid[i][j+1] == -1:
-                    return -1
-                else:
-                    return dfs(i + 1, j + 1, grid)
-            if grid[i][j] == -1:
-                if j <= 0 or grid[i][j-1] == 1:
-                    return -1
-                else:
-                    return dfs(i + 1, j - 1, grid)
-        ans = [None] * len(grid[0])
+        res = [-1]*len(grid[0])
         for j in range(len(grid[0])):
-            ans[j] = dfs(0, j, grid)
-        return ans
+            flag = True
+            newJ = j
+            for i in range(len(grid)):
+                if (newJ == 0 and grid[i][newJ]== -1) or (newJ==len(grid[0])-1 and grid[i][newJ]==1) or (newJ<len(grid[0])-1 and grid[i][newJ] == 1 and grid[i][newJ+1] == -1) or (newJ > 0 and grid[i][newJ]==-1 and grid[i][newJ-1]==1 ):
+                    res[j] = -1
+                    flag = False
+                    break
+                newJ = newJ + grid[i][newJ]
+                i+=1
+            if flag:
+                res[j] = newJ
+        return res
