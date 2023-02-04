5dd8685ca03a1534153f05551ab9298bc7db60d9
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-02-05 00:18:12 +0200

    Time: 2557 ms (14.49%), Space: 29.9 MB (35.27%) - LeetHub

---
+++ 1818-minimum-absolute-sum-difference/1818-minimum-absolute-sum-difference.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minAbsoluteSumDiff(self, nums1: List[int], nums2: List[int]) -> int:
+        sumDifference = 0
+        mod = 10**9 + 7
+        for i in range(len(nums1)):
+            sumDifference = (sumDifference + abs(nums1[i] - nums2[i]))%mod
+        sortedNums1 = sorted(nums1)
+        maxSaved = 0
+        for i in range(len(nums1)):
+            minAbsSum = curAbsSum = abs(nums1[i] - nums2[i])
+            left = 0
+            right = len(nums1) - 1
+            while left <= right:
+                mid  = (left+right)//2
+                minAbsSum = min(minAbsSum,abs(sortedNums1[mid]-nums2[i]))
+                if sortedNums1[mid] >= nums2[i]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            maxSaved = max(maxSaved,curAbsSum - minAbsSum)
+        return sumDifference - maxSaved if sumDifference - maxSaved >= 0 else sumDifference - maxSaved + mod 
