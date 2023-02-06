4b9f0f2e58a98fc0e37d90ed674cf62f0f8326fd
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-02-07 00:53:50 +0200

    Time: 37 ms (40.09%), Space: 13.8 MB (70.26%) - LeetHub

--- 0556-next-greater-element-iii/0556-next-greater-element-iii.py
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.py
@@ -1,19 +1,17 @@
 class Solution:
     def nextGreaterElement(self, n: int) -> int:
         s = [digit for digit in str(n)]
-        res = -1
-        Min = "z"
+        stack = []
         for i in range(len(s)-1,-1,-1):
-            if i == 0:
-                return -1
-            if s[i] > s[i-1]:
-                index = -1
-                for j in range(i,len(s)):
-                    if s[j] > s[i-1] and s[j] < Min:
-                        Min = s[j]
-                        index = j
-                s[index],s[i-1] = s[i-1],s[index]
-                break
-        temp = [digit for digit in s[i:]]
-        temp.sort()
-        return int("".join(s[:i]) + "".join(temp)) if int("".join(s[:i]) + "".join(temp)) <= 2**31 - 1 else -1
+            if not stack or stack[-1][1] <= s[i]:
+                stack.append([i,s[i]])
+                continue
+            while stack and stack[-1][1] > s[i]:
+                popped = stack.pop()
+            index = popped[0]
+            s[index],s[i] = s[i],s[index]
+            break
+        nextGreater = int("".join(s[:i+1]) + "".join(sorted(s[i+1:])))
+        if len(stack) != len(s) and nextGreater <= 2**31 - 1:
+            return nextGreater 
+        return -1
