1cecf6436567f535f3d1af99bc58e4800d152f2d
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-02-08 20:20:28 +0200

    Time: 288 ms (61.87%), Space: 33.9 MB (26.92%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
@@ -1,33 +1,36 @@
 class Solution:
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
-        if len(nums) < k or sum(nums)%k !=0 or max(nums) > sum(nums)/k:
+        used = [False]*len(nums)
+        total_sum = sum(nums)
+        if total_sum % k != 0:
             return False
-        newK = k
-        newNums = []
-        Sum = sum(nums)
-        for num in nums:
-            if num==Sum//k:
-                newK-=1
-            else:
-                newNums.append(num)
-        if newK == 0:
-            return True
-        newNums.sort(reverse = True )
-        self.target = sum(newNums)//newK
-        used = [False]*16
-        newNums.sort(reverse=True)
-        def dfs(i,subsetSum,k):
-            if k == 1 and subsetSum == self.target:
+        target = total_sum // k
+        nums.sort(reverse = True)
+        
+        #sorting the array in descending order
+        #so if first value is greater than target, it will not be included in any subset
+        #so we cant partition the entire array into k equal sum subsets
+        if nums[0] > target:
+            return False
+        
+        dp = {}
+        def backtrack(i,k,rem):
+            #since subproblem depends on used indices of array
+            #if same subproblem occurs again just return dp value
+            if tuple(used) in dp:
+                return dp[tuple(used)]
+            if k == 0:
                 return True
-            if i==len(newNums) or subsetSum > self.target:
-                return False
-            if subsetSum == self.target:
-                return dfs(0,0,k-1)
-            if not used[i]:
-                used[i] = True
-                if dfs(i+1,subsetSum+newNums[i],k):
-                    return True
-                used[i] = False
-            if dfs(i+1,subsetSum,k):
-                return True
-        return dfs(0,0,newK)
+            if rem == 0:
+                partition = backtrack(0,k-1,target)
+                dp[tuple(used)] = partition
+                return partition
+            for j in range(i,len(nums)):
+                if not used[j] and rem-nums[j] >= 0:
+                    used[j] = True
+                    if backtrack(j+1,k,rem-nums[j]):
+                        return True
+                    used[j] = False
+            dp[tuple(used)] = False
+            return False
+        return backtrack(0,k,target)
