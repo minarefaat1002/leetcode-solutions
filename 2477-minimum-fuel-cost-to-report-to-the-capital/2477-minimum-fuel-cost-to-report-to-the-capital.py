33e73cc5c499a8d899bec2fcd697b7dc92b3e02c
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-02-12 20:12:35 +0200

    Time: 2103 ms (48.60%), Space: 163.9 MB (34.27%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.py
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.py
@@ -1,22 +1,18 @@
 class Solution:
     def minimumFuelCost(self, roads: List[List[int]], seats: int) -> int:
         graph = collections.defaultdict(list)
-        visited = [False]*(len(roads)+1)
         for road in roads:
             graph[road[0]].append(road[1])
             graph[road[1]].append(road[0])
         res = [0]
-        def dfs(root):
-            if len(graph[root]) == 1 and root!=0:
-                return 1
-            if visited[root]:
-                return 0
-            visited[root] = True
+        def dfs(root,parent):
             Sum = 0
             for neighbour in graph[root]:
-                d = dfs(neighbour)
+                if neighbour == parent:
+                    continue
+                d = dfs(neighbour,root)
                 Sum += d
                 res[0] += math.ceil(d/seats)
             return 1 + Sum
-        dfs(0)
+        dfs(0,-1)
         return res[0]
