1835106d44dca13601a6bf5769dd91b8ca2473e1
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-02-12 20:13:05 +0200

    Time: 2098 ms (49.53%), Space: 162.7 MB (42.99%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.py
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.py
@@ -4,15 +4,16 @@ class Solution:
         for road in roads:
             graph[road[0]].append(road[1])
             graph[road[1]].append(road[0])
-        res = [0]
+        fuel = 0
         def dfs(root,parent):
+            nonlocal fuel
             Sum = 0
             for neighbour in graph[root]:
                 if neighbour == parent:
                     continue
                 d = dfs(neighbour,root)
                 Sum += d
-                res[0] += math.ceil(d/seats)
+                fuel += math.ceil(d/seats)
             return 1 + Sum
         dfs(0,-1)
-        return res[0]
+        return fuel
