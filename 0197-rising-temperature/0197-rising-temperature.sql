f914cc4caec83e4033868e79560ded321744bded
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-03-21 21:46:20 +0200

    Time: 1223 ms (11.22%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,2 +1,13 @@
 # Write your MySQL query statement below
-SELECT id FROM Weather w1 WHERE w1.temperature > (SELECT temperature FROM Weather WHERE recordDate =w1.recordDate - interval 1 day)
+# SELECT id FROM Weather w1 WHERE w1.temperature > (SELECT temperature FROM Weather WHERE recordDate =w1.recordDate - interval 1 day)
+
+
+
+
+SELECT
+    weather.id AS 'Id'
+FROM
+    weather
+        JOIN
+    weather w ON DATEDIFF(weather.recordDate, w.recordDate) = 1
+        AND weather.Temperature > w.Temperature
