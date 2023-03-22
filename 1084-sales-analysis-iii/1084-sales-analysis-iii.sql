b7b3347a87ee9c22c7dd8a239c9de5d4f534fa64
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-03-22 20:11:02 +0200

    Time: 2260 ms (20.26%), Space: 0B (100.00%) - LeetHub

--- 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -5,9 +5,19 @@
 # Using(product_id)
 # GROUP BY product_id
 # HAVING MIN(sale_date) >= '2019-01-01' AND MAX(sale_date) <= '2019-03-31' 
+
+
 select product_id,product_name
 from product p join sales s 
 using(product_id)
 group by product_id
 having COUNT(IF(sale_date<'2019-01-01', 1, NULL))=0
 and COUNT(IF(sale_date>'2019-03-31', 1, NULL))=0
+
+
+# select product_id,product_name
+# from product p join sales s 
+# using(product_id)
+# group by product_id
+# having sum(sale_date<"2019-01-01")=0
+# and sum(sale_date>"2019-03-31")=0
