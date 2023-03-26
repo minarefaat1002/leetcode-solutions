2307acf18243cb9242b48bf78a7e55d1835452aa
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-03-26 12:16:49 +0200

    Time: 470 ms (47.24%), Space: 0B (100.00%) - LeetHub

--- 0627-swap-salary/0627-swap-salary.sql
+++ 0627-swap-salary/0627-swap-salary.sql
@@ -1,3 +1,12 @@
 # Write your MySQL query statement below
 UPDATE salary
 SET sex = CASE WHEN sex LIKE 'm' THEN 'f' else 'm' end
+/*
+
+UPDATE salary
+SET
+    sex = CASE sex
+        WHEN 'm' THEN 'f'
+        ELSE 'm'
+    END;
+*/
