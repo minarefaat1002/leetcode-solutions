d2c2581b561902d641dfd706b05854acc5f6047f
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-03-26 13:45:22 +0200

    Time: 785 ms (93.60%), Space: 0B (100.00%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -3,16 +3,12 @@ SELECT id,
 "Root" AS type FROM Tree
 WHERE P_id IS NULL
 UNION 
-SELECT id,"Inner" FROM Tree T
-WHERE P_id IS NOT NULL AND (
-EXISTS (
-SELECT * FROM Tree WHERE P_id = T.id
-)
-)
+SELECT id,"Inner" AS type FROM Tree T
+WHERE (T.P_id IS NOT NULL) AND (T.id IN (
+SELECT P_id FROM Tree
+))
 UNION 
-SELECT id,"Leaf" AS type FROM Tree T 
-WHERE p_ID is not null AND (
-NOT EXISTS (
-SELECT * FROM Tree WHERE P_id = T.id
-)
-)
+SELECT id,"Leaf" AS type FROM Tree T
+WHERE (T.P_id IS NOT NULL) AND (T.id NOT IN (
+SELECT IFNULL(P_id,0) FROM Tree
+))
