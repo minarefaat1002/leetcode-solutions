c5fb10c81c42c4c12ef95feceebc9b0cd95ca1fe
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-04-08 16:14:32 +0200

    Time: 404 ms (61.19%), Space: 0B (100.00%) - LeetHub

--- 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
+++ 1661-average-time-of-process-per-machine/1661-average-time-of-process-per-machine.sql
@@ -1,3 +1,5 @@
 # Write your MySQL query statement below
-SELECT machine_id,ROUND((SUM(IF(A.activity_type="end",timestamp,0)) - SUM(IF(A.activity_type="start",timestamp,0)))/(COUNT(*)/2),3) processing_time FROM Activity A
+SELECT machine_id,
+ROUND((SUM(IF(A.activity_type="end",timestamp,0)) - SUM(IF(A.activity_type="start",timestamp,0)))/(COUNT(DISTINCT process_id)),3) processing_time 
+FROM Activity A
 GROUP BY machine_id
