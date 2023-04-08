0cc28a2f75dd069953026e8847d98c92fddd6d5e
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-04-08 22:29:02 +0200

    Time: 846 ms (95.90%), Space: 0B (100.00%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-SELECT employee_id, IF(COUNT(*) > 1, SUM(IF(primary_flag="Y",department_id,0)) , SUM(department_id)) AS department_id  FROM Employee
+SELECT employee_id, IF(COUNT(*) > 1, SUM(IF(primary_flag="Y",department_id,0)) , department_id) AS department_id  FROM Employee
 GROUP BY employee_id
