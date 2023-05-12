0a2c5b4bc7e5ff3893757c158284e2b2944794c8
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-05-12 16:48:41 +0300

    Time: 49 ms (5.82%), Space: 16.3 MB (15.24%) - LeetHub

--- 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.py
+++ 2600-k-items-with-the-maximum-sum/2600-k-items-with-the-maximum-sum.py
@@ -1,10 +1,3 @@
 class Solution:
     def kItemsWithMaximumSum(self, numOnes: int, numZeros: int, numNegOnes: int, k: int) -> int:
-        res = 0
-        res += min(k,numOnes)
-        k -= numOnes + numZeros
-        
-        if k <= 0:
-            return res
-        else:
-            return res - k
+        return numOnes-(k-numOnes-numZeros) if k> numOnes+numZeros else min(numOnes,k)
