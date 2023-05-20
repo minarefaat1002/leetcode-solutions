a3ec69946f4189ebdc9c07b801efcb52c3fafa90
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-05-20 15:55:42 +0300

    Time: 63 ms (36.00%), Space: 16.3 MB (87.41%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.py
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.py
@@ -2,5 +2,5 @@ class Solution:
     def countSeniors(self, details: List[str]) -> int:
         count = 0
         for detail in details:
-            count+= 1 if int(detail[11:13]) > 60 else 0
+            count+= 1 if detail[11:13] > '60' else 0
         return count 
