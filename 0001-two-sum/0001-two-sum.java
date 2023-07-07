0961c7bfce54e4ec500e9b1f18d6a038bd3132ed
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-07-07 18:03:37 +0300

    Time: 3 ms (63.90%), Space: 44.2 MB (8.38%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -29,6 +29,37 @@ class Solution {
     }
 }
 /*
+    public int[] twoSum(int[] nums, int target) {
+        int[] ans = new int[2];
+        int[] copy = new int[nums.length];
+        int temp1 = 0, temp2 = 0;
+        for(int i=0; i<nums.length; i++){
+            copy[i] = nums[i];
+        }
+        Arrays.sort(copy);
+        int l = 0, r = nums.length - 1;
+        while(l < r){
+            if(copy[l] + copy[r] == target){
+                temp1 = copy[l];
+                temp2 = copy[r];
+                break;
+            } else if(copy[l] + copy[r] < target){
+                l++;
+            } else {
+                r--;
+            }
+        }
+        int k = 0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] == temp1 || nums[i] == temp2){
+                ans[k++] = i;
+            }
+        }
+        return ans;
+    }
+}
+*/
+/*
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
