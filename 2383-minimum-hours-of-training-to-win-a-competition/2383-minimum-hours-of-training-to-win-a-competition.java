b49ec9b58e595f5788af27c87bc8eea5ebfcf426
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-08-07 21:39:55 +0300

    Time: 0 ms (100.00%), Space: 41.1 MB (38.99%) - LeetHub

--- 2383-minimum-hours-of-training-to-win-a-competition/2383-minimum-hours-of-training-to-win-a-competition.java
+++ 2383-minimum-hours-of-training-to-win-a-competition/2383-minimum-hours-of-training-to-win-a-competition.java
@@ -18,14 +18,3 @@ class Solution {
         return totalHours;
     }
 }
- // totalHours = 0
- //        for en,ex in zip(energy,experience):
- //            if initialEnergy <= en:
- //                totalHours += en - initialEnergy + 1
- //                initialEnergy = en + 1
- //            if initialExperience <= ex:
- //                totalHours += ex - initialExperience + 1
- //                initialExperience = ex + 1
- //            initialEnergy -= en
- //            initialExperience += ex
- //        return totalHours
