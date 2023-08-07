75021089ddb9ca88aadf6fef5cbfb6dc314912f4
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-08-07 20:04:58 +0300

    Time: 70 ms (98.29%), Space: 48.7 MB (93.29%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -3,7 +3,7 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    for(let i = 0;i<Math.floor(s.length/2);i++){
+    for(let i = 0;i<Math.trunc(s.length/2);i++){ // Math.trunc ==> removes the decimal part .
         let temp = s[i];
         s[i] = s[s.length-i-1];
         s[s.length-i-1] = temp;
