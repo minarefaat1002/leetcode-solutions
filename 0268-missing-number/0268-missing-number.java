15b6be0bf7f123ff0ae1415db61ecf7303541096
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-08-08 02:36:49 +0300

    Time: 5 ms (27.00%), Space: 44 MB (84.67%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 0; i<nums.length;i++){
+            if(i!=nums[i]) return i;
+        }
+        return nums.length;
+
+        
+        
+        
+        // int n = nums.length;
+        // int sum = n*(n+1)/2;
+        // for(int num : nums){
+        //     sum-=num;
+        // }
+        // return sum;
+    }
+}
