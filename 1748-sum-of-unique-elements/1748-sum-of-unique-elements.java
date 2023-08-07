340f949b2bc13307d54cf22e9cca171ee5ed1b62
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-08-07 13:06:07 +0300

    Time: 1 ms (82.86%), Space: 40 MB (87.49%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
@@ -1,13 +1,25 @@
 class Solution {
     public int sumOfUnique(int[] nums) {
+        // int sum = 0;
+        // int[] values = new int[101];
+        // Arrays.fill(values,0);
+        // for(int num: nums){
+        //     values[num]++;
+        // }
+        // for(int i = 0; i<values.length;i++){
+        //     if(values[i]==1) sum+=i;
+        // }
+        // return sum;
         int sum = 0;
-        int[] values = new int[101];
-        Arrays.fill(values,0);
-        for(int num: nums){
-            values[num]++;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num:nums){
+            if(map.containsKey(num)) map.put(num, map.get(num)+1);
+            else map.put(num,1);
         }
-        for(int i = 0; i<values.length;i++){
-            if(values[i]==1) sum+=i;
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            if(entry.getValue()==1){
+                sum+= entry.getKey();
+            }
         }
         return sum;
     }
