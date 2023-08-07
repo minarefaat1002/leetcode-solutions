43b40b6e6dfafa1ea603f40b78b76c266c9f4311
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-08-08 02:41:05 +0300

    Time: 53 ms (94.78%), Space: 44.3 MB (58.88%) - LeetHub

--- 0268-missing-number/0268-missing-number.js
+++ 0268-missing-number/0268-missing-number.js
@@ -12,21 +12,4 @@ var missingNumber = function(nums) {
     let sum = n*(n+1)/2;
     for(let num of nums) sum-=num;
     return sum
-    
-    
-    
-    
-//             Arrays.sort(nums);
-//         for(int i = 0; i<nums.length;i++){
-//             if(i!=nums[i]) return i;
-//         }
-//         return nums.length;
-
-        
-//         int n = nums.length;
-//         int sum = n*(n+1)/2;
-//         for(int num : nums){
-//             sum-=num;
-//         }
-//         return sum;
 };
