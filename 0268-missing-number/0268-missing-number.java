6d5135481984b2a88ae3b13ff7a86fdb7d4a8fcf
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-08-08 02:37:00 +0300

    Time: 6 ms (24.44%), Space: 44.2 MB (63.59%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -7,13 +7,11 @@ class Solution {
         return nums.length;
 
         
-        
-        
-        // int n = nums.length;
-        // int sum = n*(n+1)/2;
-        // for(int num : nums){
-        //     sum-=num;
-        // }
-        // return sum;
+//         int n = nums.length;
+//         int sum = n*(n+1)/2;
+//         for(int num : nums){
+//             sum-=num;
+//         }
+//         return sum;
     }
 }
