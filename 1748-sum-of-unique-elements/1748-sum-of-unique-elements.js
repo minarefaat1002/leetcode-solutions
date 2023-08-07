b750ab723a1dceb3b3969460c127574387e3d804
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-08-07 13:24:47 +0300

    Time: 49 ms (92.77%), Space: 42 MB (70.00%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.js
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.js
@@ -3,6 +3,17 @@
  * @return {number}
  */
 var sumOfUnique = function(nums) {
+  //    let map = {};
+  // let sum = 0;
+  // for (let num of nums) {
+  //   map[num] = map[num] + 1 || 1;
+  // }
+  // for (let key in map) {
+  //   if (map[key] === 1) {
+  //     sum += +key;
+  //   }
+  // }
+  // return sum;
     const arr = Array(101).fill(0);
     let total = 0;
     for(num of nums)arr[num]++;
