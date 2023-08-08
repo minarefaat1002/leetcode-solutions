dcbe2006a886024debe7a6c14111db67d9cb81c6
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-08-08 04:34:45 +0300

    Time: 1 ms (96.51%), Space: 40.6 MB (92.44%) - LeetHub

--- 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.java
+++ 2283-check-if-number-has-equal-digit-count-and-digit-value/2283-check-if-number-has-equal-digit-count-and-digit-value.java
@@ -3,10 +3,10 @@ class Solution {
         int[] count = new int[10];
         Arrays.fill(count,0);
         for(int i=0;i<num.length();i++){
-            count[Character.getNumericValue(num.charAt(i))]++;
+            count[num.charAt(i)-'0']++;
         }
         for(int i=0;i<num.length();i++){
-            if (Character.getNumericValue(num.charAt(i)) != count[i]) return false;
+            if (num.charAt(i)-'0' != count[i]) return false;
         }
         return true;
     }
