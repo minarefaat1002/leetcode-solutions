2d9bdb9343a212ecacdec6883e143d28db6aad17
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-08-09 22:12:33 +0300

    Time: 82 ms (79.23%), Space: 45.7 MB (99.03%) - LeetHub

--- 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.js
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.js
@@ -1,16 +1,18 @@
-class Solution {
-    public int[] sortArrayByParityII(int[] nums) {
-        int evenPointer = 0;
-        int oddPointer = 1;
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParityII = function(nums) {
+        let evenPointer = 0;
+        let oddPointer = 1;
         while(evenPointer < nums.length && oddPointer < nums.length){
             if(nums[evenPointer]%2 == 0) evenPointer+=2;
             else if(nums[oddPointer]%2 == 1) oddPointer+=2;
             else {
-                int temp = nums[evenPointer];
+                let temp = nums[evenPointer];
                 nums[evenPointer] = nums[oddPointer];
                 nums[oddPointer] = temp;
             }
         }
-        return nums;
-    }
-}
+        return nums;  
+};
