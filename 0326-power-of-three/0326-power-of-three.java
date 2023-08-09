7f82774e660de9b3a3c517b3a5ce49a3249768c0
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-08-09 15:46:02 +0300

    Time: 9 ms (84.60%), Space: 43.5 MB (5.95%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
         double temp = Math.log10(n)/Math.log10(3);
-        System.out.println(temp);
         return temp == (int) temp;
     }
 }
