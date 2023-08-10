46b535158fa19f0c3e759f4e342c3d7113af9c04
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-08-10 04:30:13 +0300

    Time: 4 ms (84.06%), Space: 44.9 MB (60.10%) - LeetHub

--- 2643-row-with-maximum-ones/2643-row-with-maximum-ones.java
+++ 2643-row-with-maximum-ones/2643-row-with-maximum-ones.java
@@ -15,17 +15,6 @@ class Solution {
             }
         }
         return new int[] {rowNumber,maxCountOnes};
-        //         maxCountOnes = 0
-        // rowNumber = -1
-        // for i in range(len(mat)):
-        //     count = 0
-        //     for j in range(len(mat[0])):
-        //         if mat[i][j] == 1:
-        //             count += 1
-        //     if count > maxCountOnes:
-        //         maxCountOnes = count 
-        //         rowNumber = i
-        // return [rowNumber ,maxCountOnes]
                 
     }
 }
