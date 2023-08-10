a8fd0bb941bd0468fa90280cbe0da4219ee0b751
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-08-10 04:55:59 +0300

    Time: 3 ms (98.76%), Space: 44.1 MB (41.18%) - LeetHub

--- 2810-faulty-keyboard/2810-faulty-keyboard.java
+++ 2810-faulty-keyboard/2810-faulty-keyboard.java
@@ -1,12 +1,5 @@
 class Solution {
     public String finalString(String s) {
-        // newString = ""
-        // for char in s:
-        //     if char == "i":
-        //         newString = newString[::-1]
-        //     else:
-        //         newString += char
-        // return newString
         StringBuilder newString = new StringBuilder("");;
         for(int i = 0;i<s.length();i++){
             char c = s.charAt(i);
