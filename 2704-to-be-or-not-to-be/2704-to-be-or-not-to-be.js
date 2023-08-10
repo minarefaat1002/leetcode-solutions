7d833f07789f6679e6f1087d3d3496d5200eb829
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-08-10 20:58:33 +0300

    Time: 49 ms (87.68%), Space: 42 MB (28.24%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    const o = Object();
+    o.val = val;
+    o.toBe = function(val){
+        if(this.val === val){
+            return true;
+        } else {
+            throw new Error("Not Equal");
+        }
+    }
+    o.notToBe = function(val){
+        if(this.val !== val){
+            return true;
+        } else {
+            throw new Error("Equal");
+        }
+    }
+    return o;
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
