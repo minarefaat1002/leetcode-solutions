7a8b4169717d47e672debac86da75c6c17bb4e45
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-08-10 19:18:21 +0300

    Time: 44 ms (84.58%), Space: 16.4 MB (50.69%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
@@ -1,19 +1,17 @@
 class Solution:
     def removeOuterParentheses(self, s: str) -> str:
-        from collections import deque
         newS = ""
-        queue = deque()
+        stack = []
         leftParenthesis = 0
         for char in s:
             if char == "(":
-                queue.append(char)
+                stack.append(char)
                 leftParenthesis+=1
             elif char == ")" and leftParenthesis == 1:
                 leftParenthesis = 0
-                queue.popleft()
-                newS += "".join(queue)
-                queue.clear()
+                newS += "".join(stack[1:])
+                stack = []
             else:
-                queue.append(char)
+                stack.append(char)
                 leftParenthesis -= 1
         return newS
