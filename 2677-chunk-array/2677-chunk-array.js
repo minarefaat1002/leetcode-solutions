5c32962a322b154d23131df06e06eb16784e43a8
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-08-11 03:10:00 +0300

    Time: 46 ms (98.40%), Space: 44.5 MB (58.27%) - LeetHub

--- 2677-chunk-array/2677-chunk-array.js
+++ 2677-chunk-array/2677-chunk-array.js
@@ -7,10 +7,12 @@ var chunk = function(arr, size) {
     let chunked = [];
     let i = 0;
     while(i<arr.length){
-        let temp = [];
-        let j = 0;
-        while(j<size && i<arr.length) temp[j++] = arr[i++];
-        chunked.push(temp);
+        // let temp = [];
+        // let j = 0;
+        // while(j<size && i<arr.length) temp[j++] = arr[i++];
+        // chunked.push(temp);
+        chunked.push(arr.slice(i,i+size));
+        i += size;
     }
     return chunked;
 };
