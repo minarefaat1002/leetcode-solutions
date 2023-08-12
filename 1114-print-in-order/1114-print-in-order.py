8f7ffb4369e3bd303456a06918a454933ba0e7cb
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-08-12 04:45:08 +0300

    Time: 3555 ms (10.28%), Space: 16.6 MB (76.11%) - LeetHub

---
+++ 1114-print-in-order/1114-print-in-order.py
@@ -0,0 +1,28 @@
+class Foo:
+    def __init__(self):
+        self.count = 0
+
+
+    def first(self, printFirst: 'Callable[[], None]') -> None:
+        
+        # printFirst() outputs "first". Do not change or remove this line.
+        if self.count == 0:
+            printFirst()
+            self.count += 1
+
+
+    def second(self, printSecond: 'Callable[[], None]') -> None:
+        
+        # printSecond() outputs "second". Do not change or remove this line.
+        while self.count != 1:
+            continue
+        printSecond()
+        self.count += 1
+
+
+    def third(self, printThird: 'Callable[[], None]') -> None:
+        
+        # printThird() outputs "third". Do not change or remove this line.
+        while self.count != 2:
+            continue
+        printThird()
