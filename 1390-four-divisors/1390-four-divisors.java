fce00fcc17ab4b9224833dae67bf0a539c896ac2
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-08-15 05:09:29 +0300

    Time: 31 ms (59.32%), Space: 43.9 MB (21.12%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.java
+++ 1390-four-divisors/1390-four-divisors.java
@@ -18,15 +18,3 @@ class Solution {
         return total;
     }
 }
-        // total = 0
-        // for num in nums:
-        //     sq = int(sqrt(num))
-        //     divisors = 0
-        //     Sum = 0
-        //     for i in range(1,sq+1):
-        //         if num%i == 0:
-        //             divisors += 2 if num/i != sq else 1
-        //             Sum += i + num//i
-        //     if divisors == 4:
-        //         total +=Sum
-        // return total 
