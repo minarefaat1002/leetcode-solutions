21c527e30881f5fc4086c9abd01f94f5ae5642b7
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-08-16 05:44:50 +0300

    Time: 1 ms (99.63%), Space: 44.7 MB (70.82%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.java
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -5,7 +5,7 @@ class Solution {
             if(i%3 == 0 && i%5 == 0) res.add("FizzBuzz");
             else if(i%3 == 0) res.add("Fizz");
             else if(i%5 == 0) res.add("Buzz");
-            else res.add(""+i+"");
+            else res.add(Integer.toString(i));
         }
         return res;
     }
