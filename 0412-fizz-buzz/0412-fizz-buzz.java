5ad187ffb5c83b77d6f96f04067683354461f45f
Mina Refaat <108765836+minarefaat1002@users.noreply.github.com>
2023-08-16 05:41:22 +0300

    Time: 1 ms (99.63%), Space: 44.6 MB (70.82%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -0,0 +1,12 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List res = new ArrayList();
+        for(int i = 1; i < n+1; i++){
+            if(i%3 == 0 && i%5 == 0) res.add("FizzBuzz");
+            else if(i%3 == 0) res.add("Fizz");
+            else if(i%5 == 0) res.add("Buzz");
+            else res.add(""+i+"");
+        }
+        return res;
+    }
+}
